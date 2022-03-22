2dc69f2d79b63ad74c382a31584e20f3118acf24
Alex Roan <alex.roan@hotmail.com>
2022-03-22 11:28:42 +0000

    Upgraded solidity used in ccip to 0.8.13

--- contracts/src/v0.8/ccip/applications/ReceiverDapp.sol
+++ contracts/src/v0.8/ccip/applications/ReceiverDapp.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.12;
+pragma solidity 0.8.13;
 
 import "../interfaces/CrossChainMessageReceiverInterface.sol";
 import "../../vendor/SafeERC20.sol";
