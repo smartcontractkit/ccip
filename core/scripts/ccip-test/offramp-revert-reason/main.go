17a385452217f96e46957a38e71aec04f9730af9
Connor Stein <connor.stein@mail.mcgill.ca>
2022-03-31 17:22:31 -0400

    Offramp revert reason script (#190)
    * Offramp revert script
    
    * Linter
    
    * chore: add extra checks for failed calls
    
    Co-authored-by: Rens Rooimans <github@rensrooimans.nl>

---
+++ core/scripts/ccip-test/offramp-revert-reason/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	"github.com/smartcontractkit/chainlink/core/internal/gethwrappers/generated/offramp"
+)
+
+func panicErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	// Need a node URL
+	// NOTE: this node needs to run in archive mode
+	ec, err := ethclient.Dial("wss://kovan.infura.io/ws/v3/myapikey")
+	panicErr(err)
+	// txHash in question
+	txHash := "0x578aea01b3ed827ec743fcd6179f81b8b0ac6b3d1ba2ea281d6fc4ec89498088"
+	tx, _, err := ec.TransactionByHash(context.Background(), common.HexToHash(txHash))
+	panicErr(err)
+	re, err := ec.TransactionReceipt(context.Background(), common.HexToHash(txHash))
+	panicErr(err)
+
+	// Random requester
+	requester := common.HexToAddress("9ca9d2d5e04012c9ed24c0e513c9bfaa4a2dd77f")
+	call := ethereum.CallMsg{
+		From:     requester,
+		To:       tx.To(),
+		Data:     tx.Data(),
+		Gas:      tx.Gas(),
+		GasPrice: tx.GasPrice(),
+	}
+	_, err = ec.CallContract(context.Background(), call, re.BlockNumber)
+	if err == nil {
+		panic("no error calling contract")
+	}
+
+	b, err := json.Marshal(err)
+	panicErr(err)
+	var callErr struct {
+		Data string `json:"data"`
+	}
+	err = json.Unmarshal(b, &callErr)
+	panicErr(err)
+
+	splits := strings.Split(callErr.Data, " ")
+
+	// Indicates an unexpected input. Parsing as an error message instead.
+	if len(splits) < 2 {
+		var msg struct {
+			Code    int    `json:"code"`
+			Message string `json:"message"`
+		}
+
+		err = json.Unmarshal(b, &msg)
+		panicErr(err)
+		panic(fmt.Sprintf("%+v", msg))
+	}
+	data, err := hex.DecodeString(splits[1][2:])
+	panicErr(err)
+	offrampABI, err := abi.JSON(strings.NewReader(offramp.OffRampABI))
+	panicErr(err)
+
+	for k, abiError := range offrampABI.Errors {
+		if bytes.Equal(data[:4], abiError.ID.Bytes()[:4]) {
+			// Found a matching error
+			v, err := abiError.Unpack(data)
+			panicErr(err)
+			fmt.Printf("Error is %v args %v\n", k, v)
+		}
+	}
+}
