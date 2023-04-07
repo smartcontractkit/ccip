b1ca47b33fe50941a54b896748b6986808d8d772
jinhoonbang <jin.bang@smartcontract.com>
2023-04-07 12:25:12 -0700

    run prettier

--- contracts/src/v0.8/metatx/Forwarder.sol
+++ contracts/src/v0.8/metatx/Forwarder.sol
@@ -16,178 +16,166 @@ import {OwnerIsCreator} from "../ccip/OwnerIsCreator.sol";
 /// @dev 2. execute() does not accept "value" parameter which allows caller to pass native token to the forwarded call
 /// @dev 3. renamed field: "address to" => "address target"
 contract Forwarder is IForwarder, ERC165, OwnerIsCreator {
-    using ECDSA for bytes32;
+  using ECDSA for bytes32;
 
-    address private constant DRY_RUN_ADDRESS = 0x0000000000000000000000000000000000000000;
+  address private constant DRY_RUN_ADDRESS = 0x0000000000000000000000000000000000000000;
 
-    string public constant GENERIC_PARAMS = "address from,address target,uint256 nonce,bytes data,uint256 validUntilTime";
+  string public constant GENERIC_PARAMS = "address from,address target,uint256 nonce,bytes data,uint256 validUntilTime";
 
-    string public constant EIP712_DOMAIN_TYPE = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
+  string public constant EIP712_DOMAIN_TYPE =
+    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
 
-    mapping(bytes32 => bool) public typeHashes;
-    mapping(bytes32 => bool) public domains;
+  mapping(bytes32 => bool) public typeHashes;
+  mapping(bytes32 => bool) public domains;
 
-    /// @notice Nonces of senders, used to prevent replay attacks
-    mapping(address => uint256) private nonces;
+  /// @notice Nonces of senders, used to prevent replay attacks
+  mapping(address => uint256) private nonces;
 
-    // solhint-disable-next-line no-empty-blocks
-    receive() external payable {}
+  // solhint-disable-next-line no-empty-blocks
+  receive() external payable {}
 
-    /// @inheritdoc IForwarder
-    function getNonce(address from)
-    public view override
-    returns (uint256) {
-        return nonces[from];
+  /// @inheritdoc IForwarder
+  function getNonce(address from) public view override returns (uint256) {
+    return nonces[from];
+  }
+
+  constructor() {
+    string memory requestType = string(abi.encodePacked("ForwardRequest(", GENERIC_PARAMS, ")"));
+    registerRequestTypeInternal(requestType);
+  }
+
+  /// @inheritdoc IERC165
+  function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC165) returns (bool) {
+    return interfaceId == type(IForwarder).interfaceId || super.supportsInterface(interfaceId);
+  }
+
+  /// @inheritdoc IForwarder
+  function verify(
+    ForwardRequest calldata req,
+    bytes32 domainSeparator,
+    bytes32 requestTypeHash,
+    bytes calldata suffixData,
+    bytes calldata sig
+  ) external view override {
+    _verifyNonce(req);
+    _verifySig(req, domainSeparator, requestTypeHash, suffixData, sig);
+  }
+
+  error ForwardFailed(bytes reason);
+
+  /// @inheritdoc IForwarder
+  function execute(
+    ForwardRequest calldata req,
+    bytes32 domainSeparator,
+    bytes32 requestTypeHash,
+    bytes calldata suffixData,
+    bytes calldata sig
+  ) external payable override returns (bool success, bytes memory ret) {
+    _verifySig(req, domainSeparator, requestTypeHash, suffixData, sig);
+    _verifyAndUpdateNonce(req);
+
+    require(req.validUntilTime == 0 || req.validUntilTime > block.timestamp, "FWD: request expired");
+
+    bytes memory callData = abi.encodePacked(req.data, req.from);
+    // solhint-disable-next-line avoid-low-level-calls
+    (success, ret) = req.target.call(callData);
+
+    if (!success) {
+      if (ret.length == 0) revert("Forwarded call reverted without reason");
+      assembly {
+        revert(add(32, ret), mload(ret))
+      }
     }
 
-    constructor() {
-        string memory requestType = string(abi.encodePacked("ForwardRequest(", GENERIC_PARAMS, ")"));
-        registerRequestTypeInternal(requestType);
+    return (success, ret);
+  }
+
+  function _verifyNonce(ForwardRequest calldata req) internal view {
+    require(nonces[req.from] == req.nonce, "FWD: nonce mismatch");
+  }
+
+  function _verifyAndUpdateNonce(ForwardRequest calldata req) internal {
+    require(nonces[req.from]++ == req.nonce, "FWD: nonce mismatch");
+  }
+
+  /// @inheritdoc IForwarder
+  function registerRequestType(string calldata typeName, string calldata typeSuffix) external override {
+    for (uint256 i = 0; i < bytes(typeName).length; i++) {
+      bytes1 c = bytes(typeName)[i];
+      require(c != "(" && c != ")", "FWD: invalid typename");
     }
 
-    /// @inheritdoc IERC165
-    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC165) returns (bool) {
-        return interfaceId == type(IForwarder).interfaceId ||
-            super.supportsInterface(interfaceId);
+    string memory requestType = string(abi.encodePacked(typeName, "(", GENERIC_PARAMS, ",", typeSuffix));
+    registerRequestTypeInternal(requestType);
+  }
+
+  function getDomainSeparator(string calldata name, string calldata version) public view returns (bytes memory) {
+    uint256 chainId;
+    /* solhint-disable-next-line no-inline-assembly */
+    assembly {
+      chainId := chainid()
     }
 
-    /// @inheritdoc IForwarder
-    function verify(
-        ForwardRequest calldata req,
-        bytes32 domainSeparator,
-        bytes32 requestTypeHash,
-        bytes calldata suffixData,
-        bytes calldata sig)
-    external override view {
-        _verifyNonce(req);
-        _verifySig(req, domainSeparator, requestTypeHash, suffixData, sig);
-    }
+    return
+      abi.encode(
+        keccak256(bytes(EIP712_DOMAIN_TYPE)),
+        keccak256(bytes(name)),
+        keccak256(bytes(version)),
+        chainId,
+        address(this)
+      );
+  }
 
-    error ForwardFailed(bytes reason);
+  /// @inheritdoc IForwarder
+  function registerDomainSeparator(string calldata name, string calldata version) external override onlyOwner {
+    bytes memory domainSeparator = getDomainSeparator(name, version);
+    bytes32 domainHash = keccak256(domainSeparator);
+    domains[domainHash] = true;
 
-    /// @inheritdoc IForwarder
-    function execute(
-        ForwardRequest calldata req,
-        bytes32 domainSeparator,
-        bytes32 requestTypeHash,
-        bytes calldata suffixData,
-        bytes calldata sig
-    )
-    external payable
-    override 
-    returns (bool success, bytes memory ret) {
-        _verifySig(req, domainSeparator, requestTypeHash, suffixData, sig);
-        _verifyAndUpdateNonce(req);
+    emit DomainRegistered(domainHash, domainSeparator);
+  }
 
-        require(req.validUntilTime == 0 || req.validUntilTime > block.timestamp, "FWD: request expired");
+  function registerRequestTypeInternal(string memory requestType) internal {
+    bytes32 requestTypehash = keccak256(bytes(requestType));
+    typeHashes[requestTypehash] = true;
+    emit RequestTypeRegistered(requestTypehash, requestType);
+  }
 
-        bytes memory callData = abi.encodePacked(req.data, req.from);
-        // solhint-disable-next-line avoid-low-level-calls
-        (success, ret) = req.target.call(callData);
+  function _verifySig(
+    ForwardRequest calldata req,
+    bytes32 domainSeparator,
+    bytes32 requestTypeHash,
+    bytes calldata suffixData,
+    bytes calldata sig
+  ) internal view virtual {
+    require(domains[domainSeparator], "FWD: unregistered domain sep.");
+    require(typeHashes[requestTypeHash], "FWD: unregistered typehash");
+    bytes32 digest = keccak256(
+      abi.encodePacked("\x19\x01", domainSeparator, keccak256(_getEncoded(req, requestTypeHash, suffixData)))
+    );
+    // solhint-disable-next-line avoid-tx-origin
 
-        if (!success) {
-            if (ret.length == 0) revert("Forwarded call reverted without reason");
-            assembly {
-                revert(add(32, ret), mload(ret))
-            }
-        }
+    require(tx.origin == DRY_RUN_ADDRESS || digest.recover(sig) == req.from, "FWD: signature mismatch");
+  }
 
-        return (success,ret);
-    }
-
-    function _verifyNonce(ForwardRequest calldata req) internal view {
-        require(nonces[req.from] == req.nonce, "FWD: nonce mismatch");
-    }
-
-    function _verifyAndUpdateNonce(ForwardRequest calldata req) internal {
-        require(nonces[req.from]++ == req.nonce, "FWD: nonce mismatch");
-    }
-
-    /// @inheritdoc IForwarder
-    function registerRequestType(string calldata typeName, string calldata typeSuffix) external override {
-
-        for (uint256 i = 0; i < bytes(typeName).length; i++) {
-            bytes1 c = bytes(typeName)[i];
-            require(c != "(" && c != ")", "FWD: invalid typename");
-        }
-
-        string memory requestType = string(abi.encodePacked(typeName, "(", GENERIC_PARAMS, ",", typeSuffix));
-        registerRequestTypeInternal(requestType);
-    }
-
-    function getDomainSeparator(string calldata name, string calldata version) public view returns (bytes memory) {
-        uint256 chainId;
-        /* solhint-disable-next-line no-inline-assembly */
-        assembly { chainId := chainid() }
-        
-        return abi.encode(
-                    keccak256(bytes(EIP712_DOMAIN_TYPE)),
-                    keccak256(bytes(name)),
-                    keccak256(bytes(version)),
-                    chainId,
-                    address(this)
-                );
-    }
-
-    /// @inheritdoc IForwarder
-    function registerDomainSeparator(string calldata name, string calldata version) onlyOwner external override {
-        bytes memory domainSeparator = getDomainSeparator(name, version);
-        bytes32 domainHash = keccak256(domainSeparator);
-        domains[domainHash] = true;
-
-        emit DomainRegistered(domainHash, domainSeparator);
-    }
-
-    function registerRequestTypeInternal(string memory requestType) internal {
-
-        bytes32 requestTypehash = keccak256(bytes(requestType));
-        typeHashes[requestTypehash] = true;
-        emit RequestTypeRegistered(requestTypehash, requestType);
-    }
-
-    function _verifySig(
-        ForwardRequest calldata req,
-        bytes32 domainSeparator,
-        bytes32 requestTypeHash,
-        bytes calldata suffixData,
-        bytes calldata sig)
-    internal
-    virtual
-    view
-    {
-        require(domains[domainSeparator], "FWD: unregistered domain sep.");
-        require(typeHashes[requestTypeHash], "FWD: unregistered typehash");
-        bytes32 digest = keccak256(abi.encodePacked(
-                "\x19\x01", domainSeparator,
-                keccak256(_getEncoded(req, requestTypeHash, suffixData))
-            ));
-        // solhint-disable-next-line avoid-tx-origin
-
-        require(tx.origin == DRY_RUN_ADDRESS || digest.recover(sig) == req.from, "FWD: signature mismatch");
-    }
-
-    /// @notice Creates a byte array that is a valid ABI encoding of a request of a `RequestType` type. See `execute()`. 
-    function _getEncoded(
-        ForwardRequest calldata req,
-        bytes32 requestTypeHash,
-        bytes calldata suffixData
-    )
-    public
-    pure
-    returns (
-        bytes memory
-    ) {
-        // we use encodePacked since we append suffixData as-is, not as dynamic param.
-        // still, we must make sure all first params are encoded as abi.encode()
-        // would encode them - as 256-bit-wide params.
-        return abi.encodePacked(
-            requestTypeHash,
-            uint256(uint160(req.from)),
-            uint256(uint160(req.target)),
-            req.nonce,
-            keccak256(req.data),
-            req.validUntilTime,
-            suffixData
-        );
-    }
+  /// @notice Creates a byte array that is a valid ABI encoding of a request of a `RequestType` type. See `execute()`.
+  function _getEncoded(
+    ForwardRequest calldata req,
+    bytes32 requestTypeHash,
+    bytes calldata suffixData
+  ) public pure returns (bytes memory) {
+    // we use encodePacked since we append suffixData as-is, not as dynamic param.
+    // still, we must make sure all first params are encoded as abi.encode()
+    // would encode them - as 256-bit-wide params.
+    return
+      abi.encodePacked(
+        requestTypeHash,
+        uint256(uint160(req.from)),
+        uint256(uint160(req.target)),
+        req.nonce,
+        keccak256(req.data),
+        req.validUntilTime,
+        suffixData
+      );
+  }
 }
