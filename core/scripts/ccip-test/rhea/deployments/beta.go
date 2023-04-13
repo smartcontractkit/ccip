fe48d3350886ca8dd98d920ea3e16bad2f60125d
connorwstein <connor.stein@mail.mcgill.ca>
2023-04-13 16:32:48 -0400

    Bump fee boost

--- core/scripts/ccip-test/rhea/deployments/beta.go
+++ core/scripts/ccip-test/rhea/deployments/beta.go
@@ -280,7 +280,7 @@ var Beta_OptimismGoerliToAvaxFuji = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.7,
+			RelativeBoostPerWaitHour: 6, // doubles every 10 minutes
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -306,7 +306,7 @@ var Beta_AvaxFujiToOptimismGoerli = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.7,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -332,7 +332,7 @@ var Beta_ArbitrumGoerliToAvaxFuji = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.7,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -358,7 +358,7 @@ var Beta_AvaxFujiToArbitrumGoerli = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.7,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -384,7 +384,7 @@ var Beta_ArbitrumGoerliToOptimismGoerli = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.7,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -410,7 +410,7 @@ var Beta_OptimismGoerliToArbitrumGoerli = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.7,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -436,7 +436,7 @@ var Beta_AvaxFujiToSepolia = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.07,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -462,7 +462,7 @@ var Beta_SepoliaToAvaxFuji = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.07,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -484,12 +484,12 @@ var Beta_OptimismGoerliToGoerli = rhea.EvmDeploymentConfig{
 		CommitOffchainConfig: ccip.CommitOffchainConfig{
 			FeeUpdateHeartBeat:    models.MustMakeDuration(24 * time.Hour),
 			FeeUpdateDeviationPPB: 5e7,
-			MaxGasPrice:           200e9,
+			MaxGasPrice:           500e9,
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.07,
-			MaxGasPrice:              200e9,
+			RelativeBoostPerWaitHour: 6,
+			MaxGasPrice:              500e9,
 		},
 	},
 }
@@ -514,7 +514,7 @@ var Beta_GoerliToOptimismGoerli = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.07,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -540,7 +540,7 @@ var Beta_OptimismGoerliToSepolia = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.07,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -566,7 +566,7 @@ var Beta_SepoliaToOptimismGoerli = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.07,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -592,7 +592,7 @@ var Beta_ArbitrumGoerliToSepolia = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.7,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
@@ -618,7 +618,7 @@ var Beta_SepoliaToArbitrumGoerli = rhea.EvmDeploymentConfig{
 		},
 		ExecOffchainConfig: ccip.ExecOffchainConfig{
 			BatchGasLimit:            5_000_000,
-			RelativeBoostPerWaitHour: 0.7,
+			RelativeBoostPerWaitHour: 6,
 			MaxGasPrice:              200e9,
 		},
 	},
