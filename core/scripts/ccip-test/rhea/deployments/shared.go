d4e68b67a161e051d1d97a73fd78ab5ba826f003
connorwstein <connor.stein@mail.mcgill.ca>
2023-05-25 16:26:51 -0400

    Boost fee boost and lower root snooze

--- core/scripts/ccip-test/rhea/deployments/shared.go
+++ core/scripts/ccip-test/rhea/deployments/shared.go
@@ -12,9 +12,13 @@ const (
 	FEE_UPDATE_HEARTBEAT                = 24 * time.Hour
 	FEE_UPDATE_DEVIATION_PPB            = 10e7 // 10%
 	FEE_UPDATE_DEVIATION_PPB_FAST_CHAIN = 20e7 // 20%
-	RELATIVE_BOOST_PER_WAIT_HOUR        = 600  // 10x every minute, fees artificially low (0.1 on testnet)
-	INFLIGHT_CACHE_EXPIRY               = 3 * time.Minute
-	ROOT_SNOOZE_TIME                    = 6 * time.Minute
+	// This boosts the fee paid every 10x every 15s, since fees artificially low (0.1 on testnet) and
+	// we have source finality artificially low. On fast chains transactions become available for execution
+	// within 30s - 1min, and we want to avoid waiting for a full root snooze so we make sure they
+	// are boosted back to cost immediately.
+	RELATIVE_BOOST_PER_WAIT_HOUR = 2400
+	INFLIGHT_CACHE_EXPIRY        = 3 * time.Minute
+	ROOT_SNOOZE_TIME             = 5 * time.Minute
 )
 
 func getBlockConfirmations(chain rhea.Chain) uint32 {
