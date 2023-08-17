9f868465d097c8a9674a4c0b912d63f9e734988e
AnieeG <anindita.ghosh@smartcontract.com>
2023-08-16 17:56:14 -0700

    get rid of toml template

--- integration-tests/testsetups/ccip.go
+++ integration-tests/testsetups/ccip.go
@@ -103,6 +103,7 @@ type CCIPTestConfig struct {
 	LocalCluster            bool
 	ExistingDeployment      bool
 	ExistingEnv             string
+	ContractsFile           string
 	ReuseContracts          bool
 	SequentialLaneAddition  bool
 	NodeFunding             *big.Float
@@ -902,7 +903,12 @@ func DeployEnvironments(
 		*/
 	}
 
-	clProps["toml"] = actions.DefaultCCIPCLNodeEnv(t, nets)
+	tomlCfg, err := node.NewConfigFromToml("ccip",
+		node.WithPrivateEVMs(nets))
+	tomlStr, err := tomlCfg.TOMLString()
+	require.NoError(t, err)
+	clProps["toml"] = tomlStr
+
 	err = testEnvironment.
 		AddHelm(chainlink.New(0, clProps)).
 		Run()
--- integration-tests/types/config/node/core.go
+++ integration-tests/types/config/node/core.go
@@ -79,6 +79,10 @@ func NewConfigFromToml(tomlFile string, opts ...NodeConfigOpt) (*chainlink.Confi
 		return nil, err
 	}
 	var cfg chainlink.Config
+	err = cfg.Validate()
+	if err != nil {
+		return nil, err
+	}
 	err = config.DecodeTOML(bytes.NewReader(b), &cfg)
 	if err != nil {
 		return nil, err
@@ -179,11 +183,7 @@ func WithPrivateEVMs(networks []blockchain.EVMNetwork) NodeConfigOpt {
 				HeadTracker: evmcfg.HeadTracker{
 					HistoryDepth: ptr(uint32(100)),
 				},
-				GasEstimator: evmcfg.GasEstimator{
-					LimitDefault:  ptr(uint32(6000000)),
-					PriceMax:      assets.GWei(200),
-					FeeCapDefault: assets.GWei(200),
-				},
+				GasEstimator: WithCCIPGasEstimator(network.ChainID),
 			},
 			Nodes: []*evmcfg.Node{
 				{
@@ -198,6 +198,27 @@ func WithPrivateEVMs(networks []blockchain.EVMNetwork) NodeConfigOpt {
 	return func(c *chainlink.Config) {
 		c.EVM = evmConfigs
 	}
+}
+
+func WithCCIPGasEstimator(chainId int64) evmcfg.GasEstimator {
+	cfg := evmcfg.GasEstimator{
+		LimitDefault:  ptr(uint32(6000000)),
+		PriceMax:      assets.GWei(200),
+		FeeCapDefault: assets.GWei(200),
+	}
+	switch chainId {
+	case 421613:
+		cfg.LimitDefault = ptr(uint32(100000000))
+	case 420:
+		cfg.BumpThreshold = ptr(uint32(60))
+		cfg.BumpPercent = ptr(uint16(20))
+		cfg.BumpMin = assets.GWei(100)
+	case 5:
+		cfg.PriceMax = assets.GWei(500)
+		cfg.FeeCapDefault = assets.GWei(500)
+	}
+
+	return cfg
 }
 
 func WithVRFv2EVMEstimator(addr string) NodeConfigOpt {
