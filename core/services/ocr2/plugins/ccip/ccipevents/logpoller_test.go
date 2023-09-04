94407163a721666646473a2423e74c6ef5f766f8
dimkouv <dimitrios.kouveris@smartcontract.com>
2023-09-04 16:20:54 +0300

    test parse func

--- core/services/ocr2/plugins/ccip/ccipevents/logpoller_test.go
+++ core/services/ocr2/plugins/ccip/ccipevents/logpoller_test.go
@@ -1,9 +1,15 @@
 package ccipevents
 
 import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/logpoller"
+	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/utils"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestLogPollerClient_loadDependency(t *testing.T) {
@@ -27,4 +33,33 @@ func TestLogPollerClient_loadDependency(t *testing.T) {
 	assert.NoError(t, err)
 	priceReg2, err := c.loadPriceRegistry(someAddr)
 	assert.True(t, priceReg == priceReg2)
+}
+
+func Test_parseLogs(t *testing.T) {
+	// generate 100 logs
+	logs := make([]logpoller.Log, 100)
+	for i := range logs {
+		logs[i].LogIndex = int64(i + 1)
+		logs[i].BlockNumber = int64(i) * 1000
+		logs[i].BlockTimestamp = time.Now()
+	}
+
+	parseFn := func(log types.Log) (*uint, error) {
+		// Simulate some random error
+		if log.Index == 100 {
+			return nil, fmt.Errorf("some error")
+		}
+		return &log.Index, nil
+	}
+
+	parsedEvents, err := parseLogs[uint](logs, logger.TestLogger(t), parseFn)
+	assert.NoError(t, err)
+	assert.Len(t, parsedEvents, 99)
+
+	// Make sure everything is parsed according to the parse func
+	for i, ev := range parsedEvents {
+		assert.Equal(t, i+1, int(ev.Data))
+		assert.Equal(t, int(i)*1000, int(ev.BlockNumber))
+		assert.Greater(t, ev.BlockTimestamp, time.Now().Add(-time.Minute))
+	}
 }
