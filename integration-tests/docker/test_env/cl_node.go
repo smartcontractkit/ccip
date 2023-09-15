9cfee16931784ea3a34bb26068ca8746dc2585cd
AnieeG <anindita.ghosh@smartcontract.com>
2023-09-14 18:47:30 -0700

    updates

--- integration-tests/docker/test_env/cl_node.go
+++ integration-tests/docker/test_env/cl_node.go
@@ -25,10 +25,11 @@ import (
 	"github.com/smartcontractkit/chainlink-testing-framework/docker"
 	"github.com/smartcontractkit/chainlink-testing-framework/docker/test_env"
 	"github.com/smartcontractkit/chainlink-testing-framework/logwatch"
-	"github.com/smartcontractkit/chainlink/v2/core/services/chainlink"
-	"github.com/smartcontractkit/chainlink/v2/core/services/keystore/chaintype"
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2/types"
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/confighelper"
+
+	"github.com/smartcontractkit/chainlink/v2/core/services/chainlink"
+	"github.com/smartcontractkit/chainlink/v2/core/services/keystore/chaintype"
 
 	"github.com/smartcontractkit/chainlink/integration-tests/client"
 	"github.com/smartcontractkit/chainlink/integration-tests/utils"
@@ -241,7 +242,7 @@ func (n *ClNode) StartContainer() error {
 	container, err := docker.StartContainerWithRetry(tc.GenericContainerRequest{
 		ContainerRequest: *cReq,
 		Started:          true,
-		Reuse:            true,
+		Reuse:            false,
 	})
 	if err != nil {
 		return errors.Wrap(err, ErrStartCLNodeContainer)
