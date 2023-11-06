2e20ae4651e524ea888d5351c86d45650a42360c
Jean Arnaud <jean.arnaud@smartcontract.com>
2023-11-06 17:41:17 +0100

    CommitStore reader test

--- core/services/ocr2/plugins/ccip/internal/ccipdata/commit_store_reader_test.go
+++ core/services/ocr2/plugins/ccip/internal/ccipdata/commit_store_reader_test.go
@@ -307,6 +307,12 @@ func TestCommitStoreReaders(t *testing.T) {
 	for v, cr := range crs {
 		cr := cr
 		t.Run("CommitStoreReader "+v, func(t *testing.T) {
+
+			// Static config.
+			cfg, err := cr.GetCommitStoreStaticConfig(context.Background())
+			require.NoError(t, err)
+			require.NotNil(t, cfg)
+
 			// Assert encoding
 			b, err := cr.EncodeCommitReport(rep)
 			require.NoError(t, err)
@@ -368,10 +374,6 @@ func TestCommitStoreReaders(t *testing.T) {
 			require.NoError(t, err)
 			assert.Equal(t, expectedGas[v], cr.GasPriceEstimator().String(gp))
 
-			// Static config.
-			cfg, err := cr.GetCommitStoreStaticConfig(context.Background())
-			require.NoError(t, err)
-			require.NotNil(t, cfg)
 		})
 	}
 }
