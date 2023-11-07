3735ed2694d4408ca6fe4313cd11472d719554f7
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2023-11-07 12:11:36 +0100

    Adds WeMix chain config (#10793)
    * Adds WeMix testnet config
    
    * Extending NoNewHeads threshold to 3s
    
    * Adds WeMix contract loader
    
    * Downgrading to go 1.20.5 bullseye
    
    * Revert "Downgrading to go 1.20.5 bullseye"
    
    This reverts commit 7eab839819456513dcf7f83ca8f08c97bdeb3835.
    
    * Update testing branch
    
    * Excludes fee delegation transactions for Wemix
    
    * Update testing branch
    
    * Linting
    
    * Fix typo
    
    * Update CTF
    
    ---------
    
    Co-authored-by: davidcauchi <david.cauchi@smartcontract.com>

--- integration-tests/contracts/contract_deployer.go
+++ integration-tests/contracts/contract_deployer.go
@@ -161,6 +161,8 @@ func NewContractDeployer(bcClient blockchain.EVMClient, logger zerolog.Logger) (
 		return &PolygonZkEvmContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	case *blockchain.KromaClient:
 		return &KromaContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
+	case *blockchain.WeMixClient:
+		return &WeMixContractDeployer{NewEthereumContractDeployer(clientImpl, logger)}, nil
 	}
 	return nil, errors.New("unknown blockchain client implementation for contract deployer, register blockchain client in NewContractDeployer")
 }
@@ -221,6 +223,10 @@ type PolygonZkEvmContractDeployer struct {
 }
 
 type KromaContractDeployer struct {
+	*EthereumContractDeployer
+}
+
+type WeMixContractDeployer struct {
 	*EthereumContractDeployer
 }
 
