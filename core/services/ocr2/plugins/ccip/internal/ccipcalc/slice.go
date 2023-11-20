8508bc5eebb82d82561030ae8d1f3f0e9ff103f0
dimkouv <dimitrios.kouveris@smartcontract.com>
2023-11-20 17:08:51 +0400

    test util func

--- core/services/ocr2/plugins/ccip/internal/ccipcalc/slice.go
+++ core/services/ocr2/plugins/ccip/internal/ccipcalc/slice.go
@@ -1,19 +1,17 @@
 package ccipcalc
 
-// TODO: test
+// FlattenUniqueSlice returns a flattened slice that contains unique elements.
 func FlattenUniqueSlice[T comparable](slices ...[]T) []T {
 	seen := make(map[T]struct{})
 	flattened := make([]T, 0)
 
 	for _, sl := range slices {
 		for _, el := range sl {
-			if _, exists := seen[el]; exists {
-				continue
+			if _, exists := seen[el]; !exists {
+				flattened = append(flattened, el)
+				seen[el] = struct{}{}
 			}
-			flattened = append(flattened, el)
-			seen[el] = struct{}{}
 		}
 	}
-
 	return flattened
 }
