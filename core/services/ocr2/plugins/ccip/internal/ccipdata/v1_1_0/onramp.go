f745c8c52a8a678b65417ce2db10354cdc83a9c7
Jean Arnaud <jean.arnaud@smartcontract.com>
2023-12-11 17:04:15 +0100

    Remove suffixes

--- core/services/ocr2/plugins/ccip/internal/ccipdata/v1_1_0/onramp.go
+++ core/services/ocr2/plugins/ccip/internal/ccipdata/v1_1_0/onramp.go
@@ -13,15 +13,15 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/ccipdata/v1_0_0"
 )
 
-var _ ccipdata.OnRampReader = &OnRampV1_1_0{}
+var _ ccipdata.OnRampReader = &OnRamp{}
 
-// OnRampV1_1_0 The only difference that the plugins care about in 1.1 is that the dynamic config struct has changed.
-type OnRampV1_1_0 struct {
+// OnRamp The only difference that the plugins care about in 1.1 is that the dynamic config struct has changed.
+type OnRamp struct {
 	*v1_0_0.OnRamp
 	onRamp *evm_2_evm_onramp_1_1_0.EVM2EVMOnRamp
 }
 
-func NewOnRamp(lggr logger.Logger, sourceSelector, destSelector uint64, onRampAddress common.Address, sourceLP logpoller.LogPoller, source client.Client) (*OnRampV1_1_0, error) {
+func NewOnRamp(lggr logger.Logger, sourceSelector, destSelector uint64, onRampAddress common.Address, sourceLP logpoller.LogPoller, source client.Client) (*OnRamp, error) {
 	onRamp, err := evm_2_evm_onramp_1_1_0.NewEVM2EVMOnRamp(onRampAddress, source)
 	if err != nil {
 		return nil, err
@@ -30,13 +30,13 @@ func NewOnRamp(lggr logger.Logger, sourceSelector, destSelector uint64, onRampAd
 	if err != nil {
 		return nil, err
 	}
-	return &OnRampV1_1_0{
+	return &OnRamp{
 		OnRamp: onRamp100,
 		onRamp: onRamp,
 	}, nil
 }
 
-func (o *OnRampV1_1_0) RouterAddress() (common.Address, error) {
+func (o *OnRamp) RouterAddress() (common.Address, error) {
 	config, err := o.onRamp.GetDynamicConfig(nil)
 	if err != nil {
 		return common.Address{}, err
@@ -44,7 +44,7 @@ func (o *OnRampV1_1_0) RouterAddress() (common.Address, error) {
 	return config.Router, nil
 }
 
-func (o *OnRampV1_1_0) GetDynamicConfig() (ccipdata.OnRampDynamicConfig, error) {
+func (o *OnRamp) GetDynamicConfig() (ccipdata.OnRampDynamicConfig, error) {
 	if o.onRamp == nil {
 		return ccipdata.OnRampDynamicConfig{}, fmt.Errorf("onramp not initialized")
 	}
