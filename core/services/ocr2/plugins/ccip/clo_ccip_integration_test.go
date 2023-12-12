535d6816ab41c4211a6ca89116f47bac71a4e9cc
Mateusz Sekara <mateusz.sekara@gmail.com>
2023-12-12 14:16:31 +0100

    Fix

--- core/services/ocr2/plugins/ccip/clo_ccip_integration_test.go
+++ core/services/ocr2/plugins/ccip/clo_ccip_integration_test.go
@@ -2,12 +2,15 @@ package ccip_test
 
 import (
 	"fmt"
+	"math/big"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/test-go/testify/require"
 
+	"github.com/smartcontractkit/chainlink/v2/core/gethwrappers/ccip/generated/router"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/testhelpers"
 	integrationtesthelpers "github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/testhelpers/integration"
 )
@@ -38,9 +41,39 @@ merge [type=merge left="{}" right="{\\\"%s\\\":$(link_parse), \\\"%s\\\":$(eth_p
 	defer linkUSD.Close()
 	defer ethUSD.Close()
 
+	// Create initial job specs
 	jobParams := ccipTH.SetUpNodesAndJobs(t, tokenPricesUSDPipeline, 19399)
 	ccipTH.SetupFeedsManager(t)
 
+	// Propose and approve new specs
 	ccipTH.ApproveJobSpecs(t, jobParams, tokenPricesUSDPipeline)
 	//ccipTH.ApproveJobSpecs(t, jobParams)
+
+	// Sanity check that CCIP works after CLO flow
+	currentSeqNum := 1
+
+	extraArgs, err := testhelpers.GetEVMExtraArgsV1(big.NewInt(200_003), false)
+	require.NoError(t, err)
+
+	msg := router.ClientEVM2AnyMessage{
+		Receiver:     testhelpers.MustEncodeAddress(t, ccipTH.Dest.Receivers[0].Receiver.Address()),
+		Data:         []byte("hello"),
+		TokenAmounts: []router.ClientEVMTokenAmount{},
+		FeeToken:     ccipTH.Source.LinkToken.Address(),
+		ExtraArgs:    extraArgs,
+	}
+	fee, err := ccipTH.Source.Router.GetFee(nil, testhelpers.DestChainSelector, msg)
+	require.NoError(t, err)
+
+	_, err = ccipTH.Source.LinkToken.Approve(ccipTH.Source.User, ccipTH.Source.Router.Address(), new(big.Int).Set(fee))
+	require.NoError(t, err)
+	ccipTH.Source.Chain.Commit()
+
+	ccipTH.SendRequest(t, msg)
+	ccipTH.AllNodesHaveReqSeqNum(t, currentSeqNum)
+	ccipTH.EventuallyReportCommitted(t, currentSeqNum)
+
+	executionLogs := ccipTH.AllNodesHaveExecutedSeqNums(t, currentSeqNum, currentSeqNum)
+	assert.Len(t, executionLogs, 1)
+	ccipTH.AssertExecState(t, executionLogs[0], testhelpers.ExecutionStateSuccess)
 }
