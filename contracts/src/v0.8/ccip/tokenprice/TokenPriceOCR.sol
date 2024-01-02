835bfe112e9284b886f370795ab62d7451d00b2d
Makram Kamaleddine <makramkd@users.noreply.github.com>
2024-01-02 13:34:11 +0200

    use OCR3base

--- contracts/src/v0.8/ccip/tokenprice/TokenPriceOCR.sol
+++ contracts/src/v0.8/ccip/tokenprice/TokenPriceOCR.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 
 import {ITypeAndVersion} from "../../shared/interfaces/ITypeAndVersion.sol";
-import {OCR2Base} from "../ocr/OCR2Base.sol";
+import {OCR3Base} from "../ocr/OCR3Base.sol";
 import {IPriceRegistry} from "../interfaces/IPriceRegistry.sol";
 import {Internal} from "../libraries/Internal.sol";
 
-contract TokenPriceOCR is ITypeAndVersion, OCR2Base {
+contract TokenPriceOCR is ITypeAndVersion, OCR3Base {
   // STATIC CONFIG
   // solhint-disable-next-line chainlink-solidity/all-caps-constant-storage-variables
   string public constant override typeAndVersion = "TokenPriceOCR 1.0.0";
@@ -22,7 +22,7 @@ contract TokenPriceOCR is ITypeAndVersion, OCR2Base {
   mapping(address token => Internal.TimestampedPackedUint224) internal s_tokenPriceUpdates;
   mapping(uint64 destChainSelector => Internal.TimestampedPackedUint224) internal s_gasPriceUpdates;
 
-  constructor(IPriceRegistry priceRegistry) OCR2Base(true) {
+  constructor(IPriceRegistry priceRegistry) OCR3Base() {
     s_priceRegistry = priceRegistry;
   }
 
@@ -30,7 +30,7 @@ contract TokenPriceOCR is ITypeAndVersion, OCR2Base {
     s_priceRegistry = priceRegistry;
   }
 
-  function _report(bytes calldata report, uint40 /* epochAndRound */) internal override {
+  function _report(bytes calldata report, uint64 /* sequenceNumber */) internal override {
     Report memory decodedReport = abi.decode(report, (Report));
     s_priceRegistry.updatePrices(Internal.PriceUpdates({
       tokenPriceUpdates: decodedReport.priceUpdates,
