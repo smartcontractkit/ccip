c3a73497b03c287bbc15297d688428b173646795
AnieeG <anindita.ghosh@smartcontract.com>
2024-01-17 15:46:05 -0800

    mock e2e usdc messenger and transmitter

---
+++ contracts/src/v0.8/ccip/test/mocks/MockE2EUSDCTokenMessenger.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.19;
+
+import {ITokenMessenger} from "../../pools/USDC/ITokenMessenger.sol";
+import {IMessageTransmitterWithRelay} from "./interfaces/IMessageTransmitterWithRelay.sol";
+import {IBurnMintERC20} from "../../../shared/token/ERC20/IBurnMintERC20.sol";
+
+// This contract mocks both the ITokenMessenger and IMessageTransmitter
+// contracts involved with the Cross Chain Token Protocol.
+contract MockUSDCTokenMessenger is ITokenMessenger {
+  uint32 private immutable i_messageBodyVersion;
+  bytes32 public constant i_destinationTokenMessenger = keccak256("i_destinationTokenMessenger");
+  uint64 public s_nonce;
+  address private i_transmitter;
+
+  // Local Message Transmitter responsible for sending and receiving messages to/from remote domains
+  IMessageTransmitterWithRelay public immutable localMessageTransmitter;
+
+  constructor(uint32 version, address transmitter) {
+    i_messageBodyVersion = version;
+    s_nonce = 1;
+    i_transmitter = transmitter;
+    localMessageTransmitter = IMessageTransmitterWithRelay(transmitter);
+  }
+
+  function depositForBurnWithCaller(
+    uint256 amount,
+    uint32 destinationDomain,
+    bytes32 mintRecipient,
+    address burnToken,
+    bytes32 destinationCaller
+  ) external returns (uint64) {
+    IBurnMintERC20(burnToken).transferFrom(msg.sender, address(this), amount);
+    IBurnMintERC20(burnToken).burn(amount);
+    // Format message body
+    bytes memory _burnMessage = abi.encodePacked(
+      i_messageBodyVersion,
+      burnToken,
+      mintRecipient,
+      amount,
+      Message.addressToBytes32(msg.sender)
+    );
+    uint64 s_nonce = _sendDepositForBurnMessage(
+      destinationDomain,
+      i_destinationTokenMessenger,
+      destinationCaller,
+      _burnMessage
+    );
+    emit DepositForBurn(
+      s_nonce,
+      burnToken,
+      amount,
+      msg.sender,
+      mintRecipient,
+      destinationDomain,
+      i_destinationTokenMessenger,
+      destinationCaller
+    );
+    return s_nonce;
+  }
+
+  function messageBodyVersion() external view returns (uint32) {
+    return i_messageBodyVersion;
+  }
+
+  function localMessageTransmitter() external view returns (address) {
+    return i_transmitter;
+  }
+
+  /**
+  * @notice Sends a BurnMessage through the local message transmitter
+     * @dev calls local message transmitter's sendMessage() function if `_destinationCaller` == bytes32(0),
+     * or else calls sendMessageWithCaller().
+     * @param _destinationDomain destination domain
+     * @param _destinationTokenMessenger address of registered TokenMessenger contract on destination domain, as bytes32
+     * @param _destinationCaller caller on the destination domain, as bytes32. If `_destinationCaller` == bytes32(0),
+     * any address can call receiveMessage() on destination domain.
+     * @param _burnMessage formatted BurnMessage bytes (message body)
+     * @return nonce unique nonce reserved by message
+     */
+  function _sendDepositForBurnMessage(
+    uint32 _destinationDomain,
+    bytes32 _destinationTokenMessenger,
+    bytes32 _destinationCaller,
+    bytes memory _burnMessage
+  ) internal returns (uint64 nonce) {
+    if (_destinationCaller == bytes32(0)) {
+      return
+      localMessageTransmitter.sendMessage(
+        _destinationDomain,
+        _destinationTokenMessenger,
+        _burnMessage
+      );
+    } else {
+      return
+      localMessageTransmitter.sendMessageWithCaller(
+        _destinationDomain,
+        _destinationTokenMessenger,
+        _destinationCaller,
+        _burnMessage
+      );
+    }
+  }
+}
--- contracts/src/v0.8/ccip/test/mocks/MockE2EUSDCTransmitter.sol
+++ contracts/src/v0.8/ccip/test/mocks/MockE2EUSDCTransmitter.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "../../pools/USDC/IMessageTransmitter.sol";
+import {IMessageTransmitterWithRelay} from "./interfaces/IMessageTransmitterWithRelay.sol";
 
-contract MockUSDCTransmitter is IMessageTransmitter {
+contract MockUSDCTransmitter is IMessageTransmitterWithRelay {
   // Indicated whether the receiveMessage() call should succeed.
   bool public s_shouldSucceed;
   uint32 private immutable i_version;
   uint32 private immutable i_localDomain;
+  // Next available nonce from this source domain
+  uint64 public nextAvailableNonce;
 
   constructor(uint32 version, uint32 localDomain) {
     i_version = version;
@@ -29,5 +31,83 @@ contract MockUSDCTransmitter is IMessageTransmitter {
 
   function localDomain() external view returns (uint32) {
     return i_localDomain;
+  }
+
+  /**
+ * @notice Send the message to the destination domain and recipient
+     * @dev Increment nonce, format the message, and emit `MessageSent` event with message information.
+     * @param destinationDomain Domain of destination chain
+     * @param recipient Address of message recipient on destination chain as bytes32
+     * @param messageBody Raw bytes content of message
+     * @return nonce reserved by message
+     */
+  function sendMessage(
+    uint32 destinationDomain,
+    bytes32 recipient,
+    bytes calldata messageBody
+  ) external override whenNotPaused returns (uint64) {
+    bytes32 _emptyDestinationCaller = bytes32(0);
+    uint64 _nonce = _reserveAndIncrementNonce();
+    bytes32 _messageSender = bytes32(uint256(uint160((msg.sender))));
+
+    _sendMessage(
+      destinationDomain,
+      recipient,
+      _emptyDestinationCaller,
+      _messageSender,
+      _nonce,
+      messageBody
+    );
+
+    return _nonce;
+  }
+
+  /**
+     * @notice Send the message to the destination domain and recipient, for a specified `destinationCaller` on the
+     * destination domain.
+     * @dev Increment nonce, format the message, and emit `MessageSent` event with message information.
+     * WARNING: if the `destinationCaller` does not represent a valid address, then it will not be possible
+     * to broadcast the message on the destination domain. This is an advanced feature, and the standard
+     * sendMessage() should be preferred for use cases where a specific destination caller is not required.
+     * @param destinationDomain Domain of destination chain
+     * @param recipient Address of message recipient on destination domain as bytes32
+     * @param destinationCaller caller on the destination domain, as bytes32
+     * @param messageBody Raw bytes content of message
+     * @return nonce reserved by message
+     */
+  function sendMessageWithCaller(
+    uint32 destinationDomain,
+    bytes32 recipient,
+    bytes32 destinationCaller,
+    bytes calldata messageBody
+  ) external override whenNotPaused returns (uint64) {
+    require(
+      destinationCaller != bytes32(0),
+      "Destination caller must be nonzero"
+    );
+
+    uint64 _nonce = _reserveAndIncrementNonce();
+    bytes32 _messageSender = bytes32(uint256(uint160((msg.sender))));
+
+    _sendMessage(
+      destinationDomain,
+      recipient,
+      destinationCaller,
+      _messageSender,
+      _nonce,
+      messageBody
+    );
+
+    return _nonce;
+  }
+
+  /**
+    * Reserve and increment next available nonce
+    * @return nonce reserved
+     */
+  function _reserveAndIncrementNonce() internal returns (uint64) {
+    uint64 _nonceReserved = nextAvailableNonce;
+    nextAvailableNonce = nextAvailableNonce + 1;
+    return _nonceReserved;
   }
 }
