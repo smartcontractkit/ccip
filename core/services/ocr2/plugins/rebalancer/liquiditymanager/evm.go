cd6536cae995cacd78fcb8c5e94a11a70219d1ea
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-01-25 16:03:54 +0200

    fix discovery

--- core/services/ocr2/plugins/rebalancer/liquiditymanager/evm.go
+++ core/services/ocr2/plugins/rebalancer/liquiditymanager/evm.go
@@ -153,7 +153,12 @@ func (e EvmRebalancer) Discover(ctx context.Context, lmFactory Factory) (*Regist
 		for destNetworkID, lmAddr := range destinationLMs {
 			netSel := destNetworkID
 
-			g.AddConnection(elem.networkSel, netSel)
+			_ = g.AddNetwork(netSel, big.NewInt(0))
+
+			_, err := g.AddConnection(elem.networkSel, netSel)
+			if err != nil {
+				return nil, nil, fmt.Errorf("add connection: %w", err)
+			}
 
 			newElem := qItem{networkSel: netSel, lmAddress: common.Address(lmAddr)}
 			if !seen.Contains(newElem) {
