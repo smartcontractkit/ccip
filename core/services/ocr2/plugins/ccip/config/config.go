526463614d871f50d602c4e015c67d59785006b5
Jean Arnaud <jean.arnaud@smartcontract.com>
2024-02-06 17:13:06 +0100

    Lint

--- core/services/ocr2/plugins/ccip/config/config.go
+++ core/services/ocr2/plugins/ccip/config/config.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
+
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/bytes"
 
 	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/utils"
