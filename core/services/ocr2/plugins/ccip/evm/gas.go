e75e736db838e44fd7038fb616945ab2a4e85eb3
Rens Rooimans <github@rensrooimans.nl>
2024-02-09 18:32:37 +0100

    fix name, use enum

--- core/services/ocr2/plugins/ccip/evm/gas.go
+++ core/services/ocr2/plugins/ccip/evm/gas.go
@@ -1,5 +1,7 @@
 package evm
 
+import chainselectors "github.com/smartcontractkit/chain-selectors"
+
 const (
 	// DefaultExecTxGasOverhead is the default gas overhead for exec transactions. This value plus the offchainconfig.BatchGasLimit
 	// should be less than the gas limit of the chain. This is enforced by the offramp reader.
@@ -10,10 +12,12 @@ const (
 // Updating a price in the price registry costs <10k gas if the asset is already in the registry. If it's a new
 // asset it will be <25k gas. There will be an OCR overhead of ~50k gas and a calldata/tx overhead of at most 50k.
 // Given these values we would be able to handle about 36 price updates for new assets or 90 for existing assets.
-func GetDefaultCommitTransactionGasLimit(chainId uint64) uint32 {
-	switch chainId {
-	case 6101244977088475029, 3478487238524512106, 4949039107694359620:
-		return 15_000_000 // Arbitrum and its testnets
+func GetDefaultCommitTransactionGasLimit(chainSelector uint64) uint32 {
+	switch chainSelector {
+	case chainselectors.ETHEREUM_MAINNET_ARBITRUM_1.Selector,
+		chainselectors.ETHEREUM_TESTNET_SEPOLIA_ARBITRUM_1.Selector,
+		chainselectors.ETHEREUM_TESTNET_GOERLI_ARBITRUM_1.Selector:
+		return 15_000_000
 	default:
 		return 1_000_000 // Default for all chains
 	}
@@ -22,11 +26,14 @@ func GetDefaultCommitTransactionGasLimit(chainId uint64) uint32 {
 // GetDefaultExecTransactionGasLimit returns the default gas limit for a transaction on a given chain.
 // The values defined here should be in sync with the offchainConfig.BatchGasLimit, which the value in
 // the offchain config always being lower than the value defined here + the gas overhead of the transaction.
-func GetDefaultExecTransactionGasLimit(chainId uint64) uint32 {
-	switch chainId {
-	case 6101244977088475029, 3478487238524512106, 4949039107694359620:
-		return 100_000_000 // Arbitrum and its testnets
+func GetDefaultExecTransactionGasLimit(chainSelector uint64) uint32 {
+	switch chainSelector {
+	case chainselectors.ETHEREUM_MAINNET_ARBITRUM_1.Selector,
+		chainselectors.ETHEREUM_TESTNET_SEPOLIA_ARBITRUM_1.Selector,
+		chainselectors.ETHEREUM_TESTNET_GOERLI_ARBITRUM_1.Selector:
+		return 100_000_000
 	default:
+
 		return 8_000_000 // Default for all chains
 	}
 }
