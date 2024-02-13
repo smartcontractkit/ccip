02fc58c4bc528ee4b3869c869ac655a0c1bcac0b
Makram Kamaleddine <makramkd@users.noreply.github.com>
2024-02-13 20:55:13 +0200

    make generate

--- core/services/ocr2/plugins/rebalancer/liquiditymanager/mocks/rebalancer_mock.go
+++ core/services/ocr2/plugins/rebalancer/liquiditymanager/mocks/rebalancer_mock.go
@@ -126,6 +126,36 @@ func (_m *Rebalancer) GetRebalancers(ctx context.Context) (map[models.NetworkSel
 	return r0, r1
 }
 
+// GetTokenAddress provides a mock function with given fields: ctx
+func (_m *Rebalancer) GetTokenAddress(ctx context.Context) (models.Address, error) {
+	ret := _m.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetTokenAddress")
+	}
+
+	var r0 models.Address
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context) (models.Address, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context) models.Address); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(models.Address)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // NewRebalancer creates a new instance of Rebalancer. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewRebalancer(t interface {
