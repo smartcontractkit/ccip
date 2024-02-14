018bbf422d572dea52191ca1ab77895dcc9427ad
Makram Kamaleddine <makramkd@users.noreply.github.com>
2024-02-14 18:36:43 +0200

    stub more tests

---
+++ core/services/ocr2/plugins/rebalancer/bridge/arb/l1_to_l2_test.go
@@ -0,0 +1,67 @@
+package arb
+
+import "testing"
+
+func TestNewL1ToL2(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_ToPendingTransfers(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_PartitionTransfers(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_FilterExecuted(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_MatchingExecutionExists(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_GetEffectiveEvents(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_ParseL1ToL2Transfers(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_ParseDepositFinalized(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_ParseLiquidityTransferred(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_QuorumizedBridgePayload(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_GetBridgePayloadAndFee(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_EstimateRetryableGasLimit(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_EstimateMaxSubmissionFee(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_EstimateMaxFeePerGasOnL2(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_ToHash(t *testing.T) {
+	// ...
+}
+
+func TestL1ToL2_EstimateAll(t *testing.T) {
+	// ...
+}
