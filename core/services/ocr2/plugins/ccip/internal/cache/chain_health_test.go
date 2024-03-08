48bd38e52c2cb007b5e3d6699eb607c24aa7bc6e
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-03-08 13:34:35 +0100

    Concurrency tests

--- core/services/ocr2/plugins/ccip/internal/cache/chain_health_test.go
+++ core/services/ocr2/plugins/ccip/internal/cache/chain_health_test.go
@@ -1,14 +1,19 @@
 package cache
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/tests"
 
+	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
 	"github.com/smartcontractkit/chainlink/v2/core/logger"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/ccipdata/mocks"
 )
@@ -172,7 +177,8 @@ func Test_ChainStateIsHealthy(t *testing.T) {
 			mockOnRamp.On("IsSourceCursed", ctx).Return(tc.onRampCursed, tc.onRampErr).Maybe()
 			mockOnRamp.On("IsSourceChainHealthy", ctx).Return(!tc.sourceChainUnhealthy, tc.sourceChainErr).Maybe()
 
-			chainState := NewChainHealthcheck(ctx, logger.TestLogger(t), mockOnRamp, mockCommitStore)
+			chainState := newChainHealthcheckWithCustomEviction(logger.TestLogger(t), mockOnRamp, mockCommitStore, 10*time.Hour, 10*time.Hour)
+
 			healthy, err := chainState.IsHealthy(ctx)
 
 			if tc.expectedErr {
@@ -183,4 +189,111 @@ func Test_ChainStateIsHealthy(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_RefreshingInBackground(t *testing.T) {
+	ctx := tests.Context(t)
+	defer ctx.Done()
+
+	mockCommitStore := newCommitStoreWrapper(t, true, nil)
+	mockCommitStore.CommitStoreReader.On("IsDestChainHealthy", mock.Anything).Return(true, nil).Maybe()
+
+	mockOnRamp := newOnRampWrapper(t, true, nil)
+	mockOnRamp.OnRampReader.On("IsSourceChainHealthy", mock.Anything).Return(true, nil).Maybe()
+
+	chainState := newChainHealthcheckWithCustomEviction(
+		logger.TestLogger(t),
+		mockOnRamp,
+		mockCommitStore,
+		10*time.Microsecond,
+		10*time.Microsecond,
+	)
+	chainState.spawnBackgroundRefresher(ctx)
+
+	// All healthy
+	assertHealthy(t, chainState, true)
+
+	// Commit store not healthy
+	mockCommitStore.set(false, nil)
+	assertHealthy(t, chainState, false)
+
+	// Commit store error
+	mockCommitStore.set(false, fmt.Errorf("commit store error"))
+	assertError(t, chainState)
+
+	// Commit store is back
+	mockCommitStore.set(true, nil)
+	assertHealthy(t, chainState, true)
+
+	// OnRamp not healthy
+	mockOnRamp.set(false, nil)
+	assertHealthy(t, chainState, false)
+
+	// OnRamp error
+	mockOnRamp.set(false, fmt.Errorf("onramp error"))
+	assertError(t, chainState)
+
+	// All back in healthy state
+	mockOnRamp.set(true, nil)
+	assertHealthy(t, chainState, true)
+}
+
+func assertHealthy(t *testing.T, ch *chainHealthcheck, expected bool) {
+	assert.Eventually(t, func() bool {
+		healthy, err := ch.IsHealthy(testutils.Context(t))
+		return err == nil && healthy == expected
+	}, testutils.WaitTimeout(t), testutils.TestInterval)
+}
+
+func assertError(t *testing.T, ch *chainHealthcheck) {
+	assert.Eventually(t, func() bool {
+		_, err := ch.IsHealthy(testutils.Context(t))
+		return err != nil
+	}, testutils.WaitTimeout(t), testutils.TestInterval)
+}
+
+type fakeStatusWrapper struct {
+	*mocks.CommitStoreReader
+	*mocks.OnRampReader
+
+	healthy bool
+	err     error
+	mu      *sync.Mutex
+}
+
+func newCommitStoreWrapper(t *testing.T, healthy bool, err error) *fakeStatusWrapper {
+	return &fakeStatusWrapper{
+		CommitStoreReader: mocks.NewCommitStoreReader(t),
+		healthy:           healthy,
+		err:               err,
+		mu:                new(sync.Mutex),
+	}
+}
+
+func newOnRampWrapper(t *testing.T, healthy bool, err error) *fakeStatusWrapper {
+	return &fakeStatusWrapper{
+		OnRampReader: mocks.NewOnRampReader(t),
+		healthy:      healthy,
+		err:          err,
+		mu:           new(sync.Mutex),
+	}
+}
+
+func (f *fakeStatusWrapper) IsDown(context.Context) (bool, error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	return !f.healthy, f.err
+}
+
+func (f *fakeStatusWrapper) IsSourceCursed(context.Context) (bool, error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	return !f.healthy, f.err
+}
+
+func (f *fakeStatusWrapper) set(healthy bool, err error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.healthy = healthy
+	f.err = err
 }
