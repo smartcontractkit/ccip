08f8897fcabdae4dc6cc53e7b67c50fb7b8d16d9
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-03-14 17:52:02 +0100

    remove waspautobuild cfg

--- integration-tests/testconfig/testconfig.go
+++ integration-tests/testconfig/testconfig.go
@@ -92,7 +92,7 @@ type TestConfig struct {
 	Network                *ctf_config.NetworkConfig        `toml:"Network"`
 	Pyroscope              *ctf_config.PyroscopeConfig      `toml:"Pyroscope"`
 	PrivateEthereumNetwork *ctf_test_env.EthereumNetwork    `toml:"PrivateEthereumNetwork"`
-	WaspConfig             *ctf_config.WaspAutoBuildConfig  `toml:"WaspAutoBuild"`
+	// WaspConfig             *ctf_config.WaspAutoBuildConfig  `toml:"WaspAutoBuild"`
 
 	Seth *seth.Config `toml:"Seth"`
 
@@ -532,11 +532,11 @@ func (c *TestConfig) Validate() error {
 		}
 	}
 
-	if c.WaspConfig != nil {
-		if err := c.WaspConfig.Validate(); err != nil {
-			return errors.Wrapf(err, "WaspAutoBuildConfig validation failed")
-		}
-	}
+	// if c.WaspConfig != nil {
+	// 	if err := c.WaspConfig.Validate(); err != nil {
+	// 		return errors.Wrapf(err, "WaspAutoBuildConfig validation failed")
+	// 	}
+	// }
 	return nil
 }
 
