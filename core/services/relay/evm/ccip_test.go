ffc10d637942976d91d817223f0a74daad611ee6
Roman Kashitsyn <roman.kashitsyn@gmail.com>
2024-03-18 22:49:59 +0100

    add a test case

--- core/services/relay/evm/ccip_test.go
+++ core/services/relay/evm/ccip_test.go
@@ -8,6 +8,11 @@ import (
 )
 
 func Test_CCIPSubjectUUID(t *testing.T) {
+	// We want the function to be
+	// (1) an actual function (i.e., deterministic)
 	assert.Equal(t, chainToUUID(big.NewInt(1)), chainToUUID(big.NewInt(1)))
+	// (2) injective (produce different results for different inputs)
 	assert.NotEqual(t, chainToUUID(big.NewInt(1)), chainToUUID(big.NewInt(2)))
+	// (3) stable across runs
+	assert.Equal(t, chainToUUID(big.NewInt(1)).String(), "c980e777-c95c-577b-83f6-ceb26a1a982d")
 }
