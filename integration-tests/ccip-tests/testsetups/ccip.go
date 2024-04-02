48cf0d5271991c69b06064ae7afda8456114d892
AnieeG <anindita.ghosh@smartcontract.com>
2024-04-02 14:24:44 -0700

    increase limit

--- integration-tests/ccip-tests/testsetups/ccip.go
+++ integration-tests/ccip-tests/testsetups/ccip.go
@@ -804,8 +804,8 @@ func CCIPDefaultTestSetUp(
 	// deploy all lane specific contracts
 	lggr.Info().Msg("Deploying chain specific contracts")
 	laneAddGrp, _ := errgroup.WithContext(setUpArgs.SetUpContext)
-	// for memory management add only 5 lanes in parallel
-	laneAddGrp.SetLimit(5)
+	// for memory management add only 20 lanes in parallel
+	laneAddGrp.SetLimit(20)
 	for _, networkPair := range testConfig.NetworkPairs {
 		n := networkPair
 		var ok bool
