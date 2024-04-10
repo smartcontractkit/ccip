527e97d92920e2d46c5b9fb47051246a448c2f06
Rens Rooimans <github@rensrooimans.nl>
2024-04-10 16:33:45 +0200

    Add explicit cov and remove from lcov
    token pool return data encoding

--- contracts/src/v0.8/ccip/libraries/Pool.sol
+++ contracts/src/v0.8/ccip/libraries/Pool.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/// @notice This library contains various token pool functions to aid constructing the return data.
 library Pool {
+  error InvalidTag(bytes4 tag);
+  error MalformedPoolReturnData(bytes data);
+
   // bytes4(keccak256("POOL_RETURN_DATA_V1_TAG"))
   bytes4 public constant POOL_RETURN_DATA_V1_TAG = 0x179fa694;
 
@@ -14,11 +18,76 @@ library Pool {
     bytes memory remotePoolAddress,
     bytes memory destPoolData
   ) internal pure returns (bytes memory) {
-    return abi.encode(PoolReturnDataV1({destPoolAddress: remotePoolAddress, destPoolData: destPoolData}));
-
-    // TODO next PR: actually use the tag
     return abi.encodeWithSelector(
       POOL_RETURN_DATA_V1_TAG, PoolReturnDataV1({destPoolAddress: remotePoolAddress, destPoolData: destPoolData})
     );
+  }
+
+  /// @notice Decodes the PoolReturnDataV1 struct from the given data. Also checks if the tag is correct.
+  /// @param encodedData The data to decode.
+  /// @dev Can revert. Since this is only used on the sending side, this is acceptable.
+  /// @return The decoded PoolReturnDataV1 struct.
+  function _decodePoolReturnDataV1(bytes memory encodedData) internal pure returns (PoolReturnDataV1 memory) {
+    if (bytes4(encodedData) != POOL_RETURN_DATA_V1_TAG) {
+      revert InvalidTag(bytes4(encodedData));
+    }
+
+    return abi.decode(_removeFirstFourBytes(encodedData), (PoolReturnDataV1));
+  }
+
+  /// @notice Removes the first four bytes from the given bytes. This can be used to undo `encodeWithSelector`.
+  /// @param _bytes The bytes to remove the first four bytes from.
+  /// @dev Can revert if the given bytes are less than four bytes long.
+  /// @return The bytes with the first four bytes removed.
+  function _removeFirstFourBytes(bytes memory _bytes) internal pure returns (bytes memory) {
+    if (_bytes.length < 4) {
+      revert MalformedPoolReturnData(_bytes);
+    }
+
+    uint256 newSliceLength = _bytes.length - 4;
+    if (newSliceLength == 0) {
+      return "";
+    }
+
+    bytes memory tempBytes;
+    assembly {
+      // Get a location of some free memory and store it in tempBytes as Solidity does for memory variables.
+      tempBytes := mload(0x40)
+
+      // Calculate length mod 32 to handle slices that are not a multiple of 32 in size.
+      let lengthmod := and(newSliceLength, 31)
+
+      // tempBytes will have the following format in memory: <length><data>
+      // When copying data we will offset the start forward to avoid allocating additional memory
+      // Therefore part of the length area will be written, but this will be overwritten later anyways.
+      // In case no offset is require, the start is set to the data region (0x20 from the tempBytes)
+      // mc will be used to keep track where to copy the data to.
+      let mc := add(add(tempBytes, lengthmod), mul(0x20, iszero(lengthmod)))
+      let end := add(mc, newSliceLength)
+
+      for {
+        // Same logic as for mc is applied and additionally the start offset specified for the method is added
+        let cc := add(add(add(_bytes, lengthmod), mul(0x20, iszero(lengthmod))), 0x4)
+      } lt(mc, end) {
+        // increase `mc` and `cc` to read the next word from memory
+        mc := add(mc, 0x20)
+        cc := add(cc, 0x20)
+      } {
+        // Copy the data from source (cc location) to the slice data (mc location)
+        mstore(mc, mload(cc))
+      }
+
+      // Store the length of the slice. This will overwrite any partial data that
+      // was copied when having slices that are not a multiple of 32.
+      mstore(tempBytes, newSliceLength)
+
+      // update free-memory pointer
+      // allocating the array padded to 32 bytes like the compiler does now
+      // To set the used memory as a multiple of 32, add 31 to the actual memory usage (mc)
+      // and remove the modulo 32 (the `and` with `not(31)`)
+      mstore(0x40, and(add(mc, 31), not(31)))
+    }
+
+    return tempBytes;
   }
 }
