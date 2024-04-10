8721682ccd4b0b14d70e52cfed4f792637e92c0c
Rens Rooimans <github@rensrooimans.nl>
2024-04-10 10:46:36 +0200

    add comments

--- contracts/src/v0.8/ccip/libraries/Pool.sol
+++ contracts/src/v0.8/ccip/libraries/Pool.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 library Pool {
+  // bytes4(keccak256("POOL_RETURN_DATA_V1_TAG"))
   bytes4 public constant POOL_RETURN_DATA_V1_TAG = 0x179fa694;
 
   struct PoolReturnDataV1 {
@@ -14,6 +15,8 @@ library Pool {
     bytes memory destPoolData
   ) internal pure returns (bytes memory) {
     return abi.encode(PoolReturnDataV1({destPoolAddress: remotePoolAddress, destPoolData: destPoolData}));
+
+    // TODO next PR: actually use the tag
     return abi.encodeWithSelector(
       POOL_RETURN_DATA_V1_TAG, PoolReturnDataV1({destPoolAddress: remotePoolAddress, destPoolData: destPoolData})
     );
