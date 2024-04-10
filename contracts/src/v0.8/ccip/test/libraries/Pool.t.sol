fd9eb6049f0642271570e039043aeccece171e71
Rens Rooimans <github@rensrooimans.nl>
2024-04-10 16:33:45 +0200

    token pool return data encoding

---
+++ contracts/src/v0.8/ccip/test/libraries/Pool.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Pool} from "../../libraries/Pool.sol";
+import {BaseTest} from "../BaseTest.t.sol";
+import "forge-std/console.sol";
+
+contract Pool__generatePoolReturnDataV1 is BaseTest {
+  function test__generatePoolReturnDataV1_Success() public view {
+    bytes memory remotePoolAddress = abi.encode(address(this));
+    bytes memory destPoolData = abi.encode(address(this));
+
+    bytes memory generatedReturnData = Pool._generatePoolReturnDataV1(remotePoolAddress, destPoolData);
+
+    Pool.PoolReturnDataV1 memory poolReturnDataV1 = Pool._decodePoolReturnDataV1(generatedReturnData);
+
+    assertEq(poolReturnDataV1.destPoolAddress, remotePoolAddress);
+    assertEq(poolReturnDataV1.destPoolData, destPoolData);
+  }
+
+  function test_fuzz__generatePoolReturnDataV1_Success(
+    bytes memory destPoolData,
+    bytes memory remotePoolAddress
+  ) public pure {
+    bytes memory generatedReturnData = Pool._generatePoolReturnDataV1(remotePoolAddress, destPoolData);
+
+    Pool.PoolReturnDataV1 memory poolReturnDataV1 = Pool._decodePoolReturnDataV1(generatedReturnData);
+
+    assertEq(poolReturnDataV1.destPoolAddress, remotePoolAddress);
+    assertEq(poolReturnDataV1.destPoolData, destPoolData);
+  }
+}
+
+contract Pool__decodePoolReturnDataV1 is BaseTest {
+  function test__decodePoolReturnDataV1_InvalidTag_Revert() public {
+    bytes memory remotePoolAddress = abi.encode(address(this));
+    bytes memory destPoolData = abi.encode(address(this));
+
+    bytes memory generatedReturnData = Pool._generatePoolReturnDataV1(remotePoolAddress, destPoolData);
+
+    generatedReturnData[0] = 0x00;
+
+    vm.expectRevert(abi.encodeWithSelector(Pool.InvalidTag.selector, bytes4(generatedReturnData)));
+    Pool._decodePoolReturnDataV1(generatedReturnData);
+  }
+}
+
+contract Pool__removeFirstFourBytes is BaseTest {
+  function test_fuzz__removeFirstFourBytes_Success(bytes calldata data) public {
+    if (data.length < 4) {
+      vm.expectRevert(abi.encodeWithSelector(Pool.MalformedPoolReturnData.selector, data));
+      Pool._removeFirstFourBytes(data);
+      return;
+    }
+
+    bytes memory result = Pool._removeFirstFourBytes(data);
+    assertEq(result, data[4:]);
+  }
+}
