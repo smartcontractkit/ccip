ab435afc28469a9a1088f4f35e6b0b2c2306a484
Jean Arnaud <jean.arnaud@smartcontract.com>
2024-04-18 13:23:04 +0200

    Lint

--- core/services/ocr2/plugins/ccip/internal/observability/onramp_observed_test.go
+++ core/services/ocr2/plugins/ccip/internal/observability/onramp_observed_test.go
@@ -9,9 +9,10 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-	cciptypes "github.com/smartcontractkit/chainlink-common/pkg/types/ccip"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+
+	cciptypes "github.com/smartcontractkit/chainlink-common/pkg/types/ccip"
 
 	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/ccipdata/mocks"
