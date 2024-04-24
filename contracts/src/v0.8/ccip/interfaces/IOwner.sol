162d16fc4269c8014cf388ddf356780a97abd4c3
Rens Rooimans <github@rensrooimans.nl>
2024-04-24 18:17:04 +0200

    add comments

--- contracts/src/v0.8/ccip/interfaces/IOwner.sol
+++ contracts/src/v0.8/ccip/interfaces/IOwner.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface IOwner {
+  /// @notice Returns the owner of the contract.
+  /// @dev This method is named to match with the OpenZeppelin Ownable contract.
   function owner() external view returns (address);
 }
