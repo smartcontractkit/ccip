b935ec14702eea0e072d276fa6dd332d917e1a82
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-04-25 09:53:49 +0200

    Missing migrations

---
+++ core/store/migrate/migrations/0231_table_test_1.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE test_table_1 (
+                              key TEXT NOT NULL,
+                              val JSONB NOT NULL,
+                              created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                              updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                              PRIMARY KEY ( key)
+);
+
+-- +goose Down
+drop table test_table_1;
+
