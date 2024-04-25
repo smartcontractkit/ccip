ce680547d6703a4bc07dd56dafc06f280a9d793b
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-04-25 18:32:36 +0200

    rm core dep

--- core/services/ocr3/plugins/ccip/commit/plugin.go
+++ core/services/ocr3/plugins/ccip/commit/plugin.go
@@ -11,7 +11,7 @@ import (
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/ocr3types"
 	"github.com/smartcontractkit/libocr/offchainreporting2plus/types"
 
-	"github.com/smartcontractkit/chainlink/v2/core/logger"
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 )
 
 // Plugin implements the main ocr3 plugin logic.
