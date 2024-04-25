1bd1ca8af58000a133d9734ef1f69bc8773b88ab
AnieeG <anindita.ghosh@smartcontract.com>
2024-04-25 23:02:30 +0530

    make maxdatabyte and batch gas limit configurable

--- integration-tests/ccip-tests/testsetups/ccip.go
+++ integration-tests/ccip-tests/testsetups/ccip.go
@@ -34,6 +34,7 @@ import (
 	"github.com/smartcontractkit/chainlink-testing-framework/k8s/environment"
 	"github.com/smartcontractkit/chainlink-testing-framework/networks"
 	"github.com/smartcontractkit/chainlink-testing-framework/utils/testcontext"
+
 	integrationactions "github.com/smartcontractkit/chainlink/integration-tests/actions"
 
 	testutils "github.com/smartcontractkit/ccip/integration-tests/ccip-tests/utils"
@@ -298,8 +299,21 @@ func (c *CCIPTestConfig) SetOCRParams() error {
 			return err
 		}
 	}
-	if c.TestGroupInput.ExecInflightExpiry != nil && c.TestGroupInput.ExecInflightExpiry.Duration() > 0 {
-		actions.InflightExpiryExec = c.TestGroupInput.ExecInflightExpiry.Duration()
+	if c.TestGroupInput.OffRampConfig != nil {
+		if c.TestGroupInput.OffRampConfig.InflightExpiry != nil &&
+			c.TestGroupInput.OffRampConfig.InflightExpiry.Duration() > 0 {
+			actions.InflightExpiryExec = c.TestGroupInput.OffRampConfig.InflightExpiry.Duration()
+		}
+		if pointer.GetUint32(c.TestGroupInput.OffRampConfig.BatchGasLimit) > 0 {
+			actions.BatchGasLimit = pointer.GetUint32(c.TestGroupInput.OffRampConfig.BatchGasLimit)
+		}
+		if pointer.GetUint32(c.TestGroupInput.OffRampConfig.MaxDataBytes) > 0 {
+			actions.MaxDataBytes = pointer.GetUint32(c.TestGroupInput.OffRampConfig.MaxDataBytes)
+		}
+		if c.TestGroupInput.OffRampConfig.RootSnooze != nil &&
+			c.TestGroupInput.OffRampConfig.RootSnooze.Duration() > 0 {
+			actions.RootSnoozeTime = c.TestGroupInput.OffRampConfig.RootSnooze.Duration()
+		}
 	}
 	if c.TestGroupInput.CommitInflightExpiry != nil && c.TestGroupInput.CommitInflightExpiry.Duration() > 0 {
 		actions.InflightExpiryCommit = c.TestGroupInput.CommitInflightExpiry.Duration()
