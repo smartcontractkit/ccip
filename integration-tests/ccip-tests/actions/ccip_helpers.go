d554593c57cc2dae4c64cd794ca47adb2fd43aad
AnieeG <anindita.ghosh@smartcontract.com>
2024-04-26 15:25:28 +0530

    fixes

--- integration-tests/ccip-tests/actions/ccip_helpers.go
+++ integration-tests/ccip-tests/actions/ccip_helpers.go
@@ -22,6 +22,7 @@ import (
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/smartcontractkit/chainlink-testing-framework/k8s/pkg/helm/mockserver"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/atomic"
@@ -3547,11 +3548,13 @@ func (c *CCIPTestEnv) ConnectToDeployedNodes() error {
 			c.nodeMutexes = append(c.nodeMutexes, &sync.Mutex{})
 		}
 		c.CLNodes = chainlinkK8sNodes
-		mockServer, err := ctfClient.ConnectMockServer(c.K8Env)
-		if err != nil {
-			return fmt.Errorf("failed to connect to mock server: %w", err)
+		if _, exists := c.K8Env.URLs[mockserver.InternalURLsKey]; exists {
+			mockServer, err := ctfClient.ConnectMockServer(c.K8Env)
+			if err != nil {
+				return fmt.Errorf("failed to connect to mock server: %w", err)
+			}
+			c.MockServer = mockServer
 		}
-		c.MockServer = mockServer
 	}
 	return nil
 }
