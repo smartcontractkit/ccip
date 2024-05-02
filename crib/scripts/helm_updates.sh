4066468de97ebe836b786ba388110001dbffef53
chainchad <96362174+chainchad@users.noreply.github.com>
2024-05-02 12:13:47 -0400

    Install helm deps from lockfile durring CRIB init

---
+++ crib/scripts/helm_updates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Update helm dependencies and builds charts for sub-charts.
+
+charts_path="../charts"
+local_charts=(chainlink-cluster)
+
+for chart in "${local_charts[@]}"; do
+  echo "Building chart for $chart from $charts_path/$chart/Chart.lock"
+  helm dependency build "$charts_path/$chart"
+done
