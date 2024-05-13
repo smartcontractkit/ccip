f10314b41fc1a18c47af6ce6ef16449375d7f49d
Josh <josh.weintraub@smartcontract.com>
2024-05-13 12:17:46 -0400

    re-organize tests

--- contracts/src/v0.8/ccip/test/mocks/test/MockRouterTest.t.sol
+++ contracts/src/v0.8/ccip/test/mocks/test/MockRouterTest.t.sol
@@ -8,6 +8,10 @@ import {Test} from "forge-std/Test.sol";
 contract MockRouterTest is Test {
   MockCCIPRouter public mockRouter;
 
+  uint64 public constant mockChainSelector = 123456;
+
+  Client.EVM2AnyMessage public message;
+
   function setUp() public {
     mockRouter = new MockCCIPRouter();
 
@@ -15,33 +19,32 @@ contract MockRouterTest is Test {
     mockRouter.setFee(0.1 ether);
 
     deal(address(this), 100 ether);
+  
+    message.receiver = abi.encode(address(0x12345));
+    message.data = abi.encode("Hello World");
   }
 
-  function test_ccipSendWithInvalidNativeTokens_Revert() public {
-    //Message with Native Token as Fee
-    Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
-      receiver: abi.encode(address(0x12345)),
-      data: abi.encode("Hello World"),
-      tokenAmounts: new Client.EVMTokenAmount[](0),
-      feeToken: address(0),
-      extraArgs: ""
-    });
-
-    uint64 mockChainSelector = 123456;
-
+  function test_ccipSendWithInsufficientNativeTokens_Revert() public {
     //Should revert because did not include sufficient eth to pay for fees
     vm.expectRevert(IRouterClient.InsufficientFeeTokenAmount.selector);
     mockRouter.ccipSend(mockChainSelector, message);
+  }
 
+  function test_ccipSendWithSufficientNativeFeeTokens_Success() public {
     //ccipSend with sufficient native tokens for fees
     mockRouter.ccipSend{value: 0.1 ether}(mockChainSelector, message);
+  }
 
-    message.feeToken = address(1); //Set feeToken to something other than native asset
-    //Should revert because msg.value should be zero when feeToken is not native asset;
+  function test_ccipSendWithInvalidMsgValue_Revert() public {
+    message.feeToken = address(1);//Set to non native-token fees
+
     vm.expectRevert(IRouterClient.InvalidMsgValue.selector);
     mockRouter.ccipSend{value: 0.1 ether}(mockChainSelector, message);
+  }
 
-    //Test a non-zero feeToken with zero msg.value. Call should succeed.
+  function test_ccipSendWithValidMsgValueAndNonNativeFeeToken_Success() public {
+    message.feeToken = address(1);//Set to non native-token fees
+
     mockRouter.ccipSend(mockChainSelector, message);
   }
 }
