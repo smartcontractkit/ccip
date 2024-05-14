7748942f92fcdd6ff8b7ab2194fe540a606ddab3
Mateusz Sekara <mateusz.sekara@gmail.com>
2024-05-14 13:19:47 +0200

    Changes to MockRMN to support per lane cursing and writing simple tests for the onramp to verify that

--- contracts/src/v0.8/ccip/test/mocks/MockRMN.sol
+++ contracts/src/v0.8/ccip/test/mocks/MockRMN.sol
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.24;
 
@@ -11,6 +12,7 @@ contract MockRMN is IRMN, OwnerIsCreator {
   bool private s_curse;
   bytes private s_err;
   RMN.VersionedConfig private s_versionedConfig;
+  mapping(bytes32 => bool) private s_subjectCurses;
 
   function isCursed() external view override returns (bool) {
     if (s_err.length != 0) {
@@ -19,23 +21,31 @@ contract MockRMN is IRMN, OwnerIsCreator {
     return s_curse;
   }
 
-  function isCursed(bytes32 /* subject */ ) external view override returns (bool) {
+  function isCursed(bytes32 subject) external view override returns (bool) {
     if (s_err.length != 0) {
       revert CustomError(s_err);
     }
-    return s_curse;
+    return s_curse || s_subjectCurses[subject];
   }
 
-  function voteToCurse(bytes32) external {
+  function voteToCurse() external {
     s_curse = true;
   }
 
-  function setRevert(bytes memory err) external {
-    s_err = err;
+  function voteToCurse(bytes32 subject) external {
+    s_subjectCurses[subject] = true;
   }
 
   function ownerUnvoteToCurse(RMN.UnvoteToCurseRecord[] memory) external {
     s_curse = false;
+  }
+
+  function ownerUnvoteToCurse(RMN.UnvoteToCurseRecord[] memory, bytes32 subject) external {
+    s_subjectCurses[subject] = false;
+  }
+
+  function setRevert(bytes memory err) external {
+    s_err = err;
   }
 
   function isBlessed(IRMN.TaggedRoot calldata) external view override returns (bool) {
