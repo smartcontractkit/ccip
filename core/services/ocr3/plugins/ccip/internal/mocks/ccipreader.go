eb26a82ec6dcd3d96fe9d5173e11cd583e1ce3b1
dimkouv <dimitrios.kouveris@smartcontract.com>
2024-05-30 11:27:05 +0200

    implement mock ccip reader interface

--- core/services/ocr3/plugins/ccip/internal/mocks/ccipreader.go
+++ core/services/ocr3/plugins/ccip/internal/mocks/ccipreader.go
@@ -19,9 +19,9 @@ func NewCCIPReader() *CCIPReader {
 	}
 }
 
-func (r CCIPReader) ReportsFromBlockNum(ctx context.Context, chain model.ChainSelector, blockNum uint64, limit int) (map[model.ChainSelector][]model.CommitReport, error) {
-	args := r.Called(ctx, chain, blockNum, limit)
-	return args.Get(0).(map[model.ChainSelector][]model.CommitReport), args.Error(1)
+func (r CCIPReader) CommitReportsGTETimestamp(ctx context.Context, dest model.ChainSelector, ts time.Time, limit int) ([]model.CommitPluginReport, error) {
+	args := r.Called(ctx, dest, ts, limit)
+	return args.Get(0).([]model.CommitPluginReport), args.Error(1)
 }
 
 func (r CCIPReader) MsgsAfterTimestamp(ctx context.Context, chains []model.ChainSelector, ts time.Time, limit int) ([]model.CCIPMsg, error) {
