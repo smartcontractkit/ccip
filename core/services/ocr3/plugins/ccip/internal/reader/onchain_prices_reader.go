813669d3214fab2c54a8c947c157b15d5daeb14b
asoliman <abdelrahman.soliman@smartcontract.com>
2024-06-14 14:34:49 +0400

    Get prices in parallel

--- core/services/ocr3/plugins/ccip/internal/reader/onchain_prices_reader.go
+++ core/services/ocr3/plugins/ccip/internal/reader/onchain_prices_reader.go
@@ -3,15 +3,16 @@ package reader
 import (
 	"context"
 	"fmt"
+	"math/big"
+	"sync"
 
 	commontypes "github.com/smartcontractkit/chainlink-common/pkg/types"
-	cciptypes "github.com/smartcontractkit/chainlink-common/pkg/types/ccipocr3"
 	ocrtypes "github.com/smartcontractkit/libocr/offchainreporting2plus/types"
 )
 
 type TokenPriceConfig struct {
 	// This is mainly used for tokens on testnet to give them a price
-	StaticPrices map[ocrtypes.Account]*cciptypes.BigInt `json:"staticPrice"`
+	StaticPrices map[ocrtypes.Account]*big.Int `json:"staticPrice"`
 }
 
 type OnchainTokenPricesReader struct {
@@ -21,29 +22,57 @@ type OnchainTokenPricesReader struct {
 }
 
 // GetTokenPricesUSD TODO: Update interface to return cciptypes.BigInt
-func (pr *OnchainTokenPricesReader) GetTokenPricesUSD(ctx context.Context, tokens []ocrtypes.Account) ([]*cciptypes.BigInt, error) {
+func (pr *OnchainTokenPricesReader) GetTokenPricesUSD(ctx context.Context, tokens []ocrtypes.Account) ([]*big.Int, error) {
 	const (
-		contractName = "PriceRegistry"
+		contractName = "PriceAggregator"
 		functionName = "getTokenPrice"
 	)
 
-	var prices []*cciptypes.BigInt
-	for _, token := range tokens {
-		var price *cciptypes.BigInt
-		if staticPrice, exists := pr.TokenPriceConfig.StaticPrices[token]; exists {
-			price = staticPrice
-		} else {
-			err := pr.contractReader.GetLatestValue(ctx, contractName, functionName, token, &price)
-			if err != nil {
-				return nil, err
-			}
-		}
+	prices := make([]*big.Int, 0, len(tokens))
+	errChan := make(chan error, 1)
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-		prices = append(prices, price)
+	var wg sync.WaitGroup
+	wg.Add(len(tokens))
+
+	for idx, token := range tokens {
+		go func(idx int, token ocrtypes.Account) {
+			defer wg.Done()
+			var price *big.Int
+			if staticPrice, exists := pr.TokenPriceConfig.StaticPrices[token]; exists {
+				price = staticPrice
+			} else {
+				if err := pr.contractReader.GetLatestValue(ctx, contractName, functionName, token, &price); err != nil {
+					select {
+					case errChan <- err:
+					default:
+					}
+					cancel()
+					return
+				}
+			}
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				prices[idx] = price
+			}
+		}(idx, token)
+	}
+
+	// Wait for all goroutines to complete
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	if err, ok := <-errChan; ok {
+		return nil, fmt.Errorf("failed to get all token prices successfully: %v", err)
 	}
 
 	if len(prices) != len(tokens) {
-		return nil, fmt.Errorf("failed to get all token prices")
+		return nil, fmt.Errorf("failed to get all token prices successfully")
 	}
 
 	return prices, nil
