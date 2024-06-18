cfdf4d5fc4c1fb81cd5ee53d47bd1ee58a592880
connorwstein <connor.stein@mail.mcgill.ca>
2024-06-18 14:38:22 -0400

    Lint

--- core/services/ocr3/plugins/ccip/commit/plugin_e2e_test.go
+++ core/services/ocr3/plugins/ccip/commit/plugin_e2e_test.go
@@ -2,9 +2,10 @@ package commit
 
 import (
 	"context"
-	"github.com/stretchr/testify/require"
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/stretchr/testify/assert"
 
