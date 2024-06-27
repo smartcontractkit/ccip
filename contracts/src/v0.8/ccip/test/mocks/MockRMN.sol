2c64d1910cd469bcff6c3ac67108f20835e6b38a
Kostis Karantias <kkarantias@gmail.com>
2024-06-27 13:53:34 +0300

    revert MockRMN to ccip-develop version
    9075d9b2aac6036b8a70dbbe6ef8a135574851a2

--- contracts/src/v0.8/ccip/test/mocks/MockRMN.sol
+++ contracts/src/v0.8/ccip/test/mocks/MockRMN.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.24;
 
-import {RMN} from "src/RMN.sol";
-import {IRMN} from "src/IRMN.sol";
-import {OwnerIsCreator} from "src/OwnerIsCreator.sol";
+import {RMN} from "../../RMN.sol";
+import {IRMN} from "../../interfaces/IRMN.sol";
+import {OwnerIsCreator} from "./../../../shared/access/OwnerIsCreator.sol";
 
 contract MockRMN is IRMN, OwnerIsCreator {
   error CustomError(bytes err);
@@ -11,32 +11,40 @@ contract MockRMN is IRMN, OwnerIsCreator {
   bool private s_curse;
   bytes private s_err;
   RMN.VersionedConfig private s_versionedConfig;
+  mapping(bytes16 subject => bool cursed) private s_curseBySubject;
 
-  function isCursed() public view override returns (bool) {
+  function isCursed() external view override returns (bool) {
     if (s_err.length != 0) {
       revert CustomError(s_err);
     }
     return s_curse;
   }
 
-  function isCursed(bytes16) external view override returns (bool) {
-    return isCursed();
+  function isCursed(bytes16 subject) external view override returns (bool) {
+    if (s_err.length != 0) {
+      revert CustomError(s_err);
+    }
+    return s_curse || s_curseBySubject[subject];
   }
 
-  function voteToCurse(bytes32) public {
+  function voteToCurse(bytes32) external {
     s_curse = true;
   }
 
-  function voteToCurse(bytes16 curseId, bytes16[] memory) external {
-    voteToCurse(curseId);
+  function voteToCurse(bytes32, bytes16 subject) external {
+    s_curseBySubject[subject] = true;
+  }
+
+  function ownerUnvoteToCurse(RMN.UnvoteToCurseRecord[] memory) external {
+    s_curse = false;
+  }
+
+  function ownerUnvoteToCurse(RMN.UnvoteToCurseRecord[] memory, bytes16 subject) external {
+    s_curseBySubject[subject] = false;
   }
 
   function setRevert(bytes memory err) external {
     s_err = err;
-  }
-
-  function ownerUnvoteToCurse(RMN.OwnerUnvoteToCurseRequest[] memory) external {
-    s_curse = false;
   }
 
   function isBlessed(IRMN.TaggedRoot calldata) external view override returns (bool) {
@@ -44,8 +52,6 @@ contract MockRMN is IRMN, OwnerIsCreator {
   }
 
   function getConfigDetails() external view returns (uint32 version, uint32 blockNumber, RMN.Config memory config) {
-    version = s_versionedConfig.configVersion;
-    blockNumber = s_versionedConfig.blockNumber;
-    config = s_versionedConfig.config;
+    return (s_versionedConfig.configVersion, s_versionedConfig.blockNumber, s_versionedConfig.config);
   }
 }
