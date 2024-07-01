e99a943e72e8f813392cb05fc0e2fcc677053326
asoliman <abdelrahman.soliman@smartcontract.com>
2024-07-01 17:34:56 +0400

    Add homechain integration test

---
+++ core/services/ocr3/plugins/ccip_integration_tests/integration_helpers.go
@@ -0,0 +1,65 @@
+package ccip_integration_tests
+
+import (
+	"context"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
+	types2 "github.com/smartcontractkit/chainlink-common/pkg/types"
+	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/client"
+	"github.com/smartcontractkit/chainlink/v2/core/chains/evm/logpoller"
+	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils"
+	"github.com/smartcontractkit/chainlink/v2/core/internal/testutils/pgtest"
+	"github.com/smartcontractkit/chainlink/v2/core/logger"
+	"github.com/smartcontractkit/chainlink/v2/core/services/relay/evm"
+	evmtypes "github.com/smartcontractkit/chainlink/v2/core/services/relay/evm/types"
+	"github.com/stretchr/testify/require"
+)
+
+const chainID = 1337
+
+type TestSetupData[T any] struct {
+	ContractAddr common.Address
+	Contract     *T
+	SimulatedBE  *backends.SimulatedBackend
+	Auth         *bind.TransactOpts
+	ChainReader  *evm.ChainReaderService
+	ChainID      int
+}
+
+func SetupChainReader(t *testing.T, simulatedBackend *backends.SimulatedBackend, address common.Address, chainReaderConfig evmtypes.ChainReaderConfig, contractName string) evm.ChainReaderService {
+	lggr := logger.TestLogger(t)
+	db := pgtest.NewSqlxDB(t)
+	lpOpts := logpoller.Opts{
+		PollPeriod:               time.Millisecond,
+		FinalityDepth:            1,
+		BackfillBatchSize:        1,
+		RpcBatchSize:             1,
+		KeepFinalizedBlocksDepth: 10000,
+	}
+	cl := client.NewSimulatedBackendClient(t, simulatedBackend, big.NewInt(chainID))
+	lp := logpoller.NewLogPoller(logpoller.NewORM(big.NewInt(chainID), db, lggr), cl, lggr, lpOpts)
+	require.NoError(t, lp.Start(testutils.Context(t)))
+
+	cr, err := evm.NewChainReaderService(context.Background(), lggr, lp, cl, chainReaderConfig)
+	require.NoError(t, err)
+	err = cr.Bind(context.Background(), []types2.BoundContract{
+		{
+			Address: address.String(),
+			Name:    contractName,
+			Pending: false,
+		},
+	})
+	require.NoError(t, err)
+	require.NoError(t, cr.Start(context.Background()))
+	for {
+		if err := cr.Ready(); err == nil {
+			break
+		}
+	}
+	return cr
+}
