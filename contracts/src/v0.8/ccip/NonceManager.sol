9f59ee72cc722bf89209e817a50e05410a282ca7
Ryan <80392855+RayXpub@users.noreply.github.com>
2024-07-02 09:01:05 +0100

    chore: reorder imports

--- contracts/src/v0.8/ccip/NonceManager.sol
+++ contracts/src/v0.8/ccip/NonceManager.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.24;
 
-import {AuthorizedCallers} from "../shared/access/AuthorizedCallers.sol";
 import {IEVM2AnyOnRamp} from "./interfaces/IEVM2AnyOnRamp.sol";
 import {INonceManager} from "./interfaces/INonceManager.sol";
+
+import {AuthorizedCallers} from "../shared/access/AuthorizedCallers.sol";
 
 /// @title NonceManager
 /// @notice NonceManager contract that manages sender nonces for the on/off ramps
