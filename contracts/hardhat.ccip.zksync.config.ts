6400d8d6c6202cc869d665eece1ab4a9a4adccce
AnieeG <anindita.ghosh@smartcontract.com>
2024-08-08 13:43:39 -0700

    add script for zksync compile contract

---
+++ contracts/hardhat.ccip.zksync.config.ts
@@ -0,0 +1,122 @@
+import '@nomicfoundation/hardhat-ethers'
+import '@nomicfoundation/hardhat-verify'
+import '@nomicfoundation/hardhat-chai-matchers'
+import "@matterlabs/hardhat-zksync-solc";
+import '@typechain/hardhat'
+import 'hardhat-abi-exporter'
+import { subtask } from 'hardhat/config'
+import { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } from 'hardhat/builtin-tasks/task-names'
+
+const COMPILER_SETTINGS = {
+  optimizer: {
+    enabled: true,
+    runs: 1000000,
+  },
+  metadata: {
+    bytecodeHash: 'none',
+  },
+}
+
+// prune forge style tests from hardhat paths
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
+    async (_, __, runSuper) => {
+      const paths = await runSuper()
+      const noTests = paths.filter((p: string) => !p.endsWith('.t.sol'))
+      return noTests.filter(
+          (p: string) => !p.includes('src/v0.8/vendor/forge-std'),
+      )
+    },
+)
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+let config = {
+  abiExporter: {
+    path: './abi',
+    runOnCompile: true,
+  },
+  paths: {
+    artifacts: './artifacts',
+    cache: './cache',
+    sources: './src/v0.8/ccip',
+    tests: './test/v0.8/ccip',
+  },
+  typechain: {
+    outDir: './typechain',
+    target: 'ethers-v5',
+  },
+  defaultNetwork: "zkSync",
+  networks: {
+    env: {
+      url: process.env.NODE_HTTP_URL || '',
+    },
+    hardhat: {
+      allowUnlimitedContractSize: Boolean(
+          process.env.ALLOW_UNLIMITED_CONTRACT_SIZE,
+      ),
+      hardfork: 'merge',
+    },
+    zkSync: {
+      url: "https://sepolia.era.zksync.dev", // The testnet RPC URL of zkSync Era network.
+      ethNetwork: "sepolia", // The Ethereum Web3 RPC URL, or the identifier of the network (e.g. `mainnet` or `sepolia`)
+      zksync: true, // enables zksolc compiler
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.6',
+        settings: COMPILER_SETTINGS,
+      },
+      {
+        version: '0.8.16',
+        settings: COMPILER_SETTINGS,
+      },
+      {
+        version: '0.8.19',
+        settings: COMPILER_SETTINGS,
+      },
+      {
+        version: '0.8.24',
+        settings: {
+          ...COMPILER_SETTINGS,
+          evmVersion: 'paris',
+        },
+      },
+    ],
+  },
+  zksolc: {
+    settings: {
+      compilerPath: "zksolc",  // optional. Ignored for compilerSource "docker". Can be used if compiler is located in a specific folder
+      optimizer: {
+        enabled: true, // optional. True by default
+        mode: '3', // optional. 3 by default, z to optimize bytecode size
+        fallback_to_optimizing_for_size: false, // optional. Try to recompile with optimizer mode "z" if the bytecode is too large
+      },
+      experimental: {
+        dockerImage: '', // deprecated
+        tag: ''   // deprecated
+      },
+     // contractsToCompile: ['RMN', 'ARMProxy'] // uncomment this to compile only specific contracts
+    }
+  },
+  mocha: {
+    timeout: 150000,
+    forbidOnly: Boolean(process.env.CI),
+  },
+  warnings: !process.env.HIDE_WARNINGS,
+}
+
+if (process.env.NETWORK_NAME && process.env.EXPLORER_API_KEY) {
+  config = {
+    ...config,
+    etherscan: {
+      apiKey: {
+        [process.env.NETWORK_NAME]: process.env.EXPLORER_API_KEY,
+      },
+    },
+  }
+}
+
+export default config
