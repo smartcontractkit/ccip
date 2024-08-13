07f8c21b03cdfa346c89a7c27b66ca6bf5940703
AnieeG <anindita.ghosh@smartcontract.com>
2024-08-12 18:06:46 -0700

    zksync compile

---
+++ contracts/hardhat.config.ccip.zksync.ts
@@ -0,0 +1,107 @@
+import '@nomicfoundation/hardhat-ethers'
+import '@nomicfoundation/hardhat-verify'
+import '@nomicfoundation/hardhat-chai-matchers'
+import '@matterlabs/hardhat-zksync-solc'
+import '@typechain/hardhat'
+import 'hardhat-abi-exporter'
+import { subtask } from 'hardhat/config'
+import { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } from 'hardhat/builtin-tasks/task-names'
+
+const COMPILER_SETTINGS = {
+  optimizer: {
+    enabled: true,
+    runs: 1000000,
+  },
+  metadata: {
+    bytecodeHash: 'none',
+  },
+}
+
+// prune forge style tests from hardhat paths
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
+    async (_, __, runSuper) => {
+      const paths = await runSuper()
+      const noTests = paths.filter((p: string) => !p.endsWith('.t.sol'))
+      return noTests.filter(
+          (p: string) => !p.includes('src/v0.8/vendor/forge-std'),
+      )
+    },
+)
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+let config = {
+  abiExporter: {
+    path: './abi',
+    runOnCompile: true,
+  },
+  paths: {
+    artifacts: './artifacts',
+    cache: './cache',
+    sources: './src/v0.8/ccip',
+    tests: './src/v0.8/ccip/test',
+  },
+  typechain: {
+    outDir: './typechain',
+    target: 'ethers-v5',
+  },
+  defaultNetwork: 'zkSync',
+  networks: {
+    env: {
+      url: process.env.NODE_HTTP_URL || '',
+    },
+    hardhat: {
+      allowUnlimitedContractSize: Boolean(
+          process.env.ALLOW_UNLIMITED_CONTRACT_SIZE,
+      ),
+      hardfork: 'merge',
+    },
+    zkSync: {
+      url: 'https://sepolia.era.zksync.dev',
+      ethNetwork: 'sepolia',
+      zksync: true, // enables zksolc compiler
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.24',
+        settings: {
+          ...COMPILER_SETTINGS,
+          evmVersion: 'paris',
+        },
+      },
+    ],
+  },
+  zksolc: {
+    settings: {
+      version : '1.4.1 ',
+      compilerPath: 'zksolc',
+      optimizer: {
+        enabled: true,
+        mode: '3',
+        fallback_to_optimizing_for_size: false,
+      },
+      experimental: {
+        dockerImage: '',
+        tag: '',
+      },
+      contractsToCompile: ['RMN', 'ARMProxy'] // uncomment this to compile only specific contracts
+    },
+  },
+  warnings: !process.env.HIDE_WARNINGS,
+}
+
+if (process.env.NETWORK_NAME && process.env.EXPLORER_API_KEY) {
+  config = {
+    ...config,
+    etherscan: {
+      apiKey: {
+        [process.env.NETWORK_NAME]: process.env.EXPLORER_API_KEY,
+      },
+    },
+  }
+}
+
+export default config
