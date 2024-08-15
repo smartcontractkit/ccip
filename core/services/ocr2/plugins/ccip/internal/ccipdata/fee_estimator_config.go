e7557ee99110f921a720efa9ff3e2ac3de9d6506
valerii.kabisov <valerii.kabisov@smartcontract.com>
2024-08-15 15:44:34 +0300

    [CCIP-2590] mock fix

--- core/services/ocr2/plugins/ccip/internal/ccipdata/fee_estimator_config.go
+++ core/services/ocr2/plugins/ccip/internal/ccipdata/fee_estimator_config.go
@@ -4,7 +4,6 @@ import (
 	"context"
 )
 
-//go:generate mockery --quiet --name FeeEstimatorConfigReader --filename fee_estimator_config_reader_mock.go --case=underscore
 type FeeEstimatorConfigReader interface {
 	GetDataAvailabilityConfig(ctx context.Context) (destDAOverheadGas, destGasPerDAByte, destDAMultiplierBps int64, err error)
 }
