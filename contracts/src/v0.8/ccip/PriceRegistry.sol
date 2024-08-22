2cd52660e51aa665d28fb19217aee0376e423d8b
0xsuryansh <suryansh.shrivastava@smartcontract.com>
2024-08-22 19:40:10 +0530

    fix : natspec comment for _calculateRebasedValue
    Signed-off-by: 0xsuryansh <suryansh.shrivastava@smartcontract.com>

--- contracts/src/v0.8/ccip/PriceRegistry.sol
+++ contracts/src/v0.8/ccip/PriceRegistry.sol
@@ -646,7 +646,7 @@ contract PriceRegistry is
   /// @param dataFeedDecimal decimal of the data feed
   /// @param tokenDecimal decimal of the token
   /// @param feedValue value of the data feed
-  /// @return rebasedVal rebased value
+  /// @return rebasedValue rebased value
   function _calculateRebasedValue(
     uint8 dataFeedDecimal,
     uint8 tokenDecimal,
