b7627f5797059ac750560217ecfe3c627caed4c5
Ryan Hall <hall.ryan.r@gmail.com>
2024-09-06 15:46:06 -0400

    add RMNRemote constructor test

--- contracts/src/v0.8/ccip/test/rmn/RMNRemote.t.sol
+++ contracts/src/v0.8/ccip/test/rmn/RMNRemote.t.sol
@@ -2,10 +2,20 @@
 pragma solidity 0.8.24;
 
 import {Internal} from "../../libraries/Internal.sol";
+import {RMNRemote} from "../../rmn/RMNRemote.sol";
 import {BaseTest} from "../BaseTest.t.sol";
 
 contract RMNRemoteTest is BaseTest {
+  RMNRemote public s_rmnRemote;
+
   function setUp() public virtual override {
     super.setUp();
+    s_rmnRemote = new RMNRemote(1);
+  }
+}
+
+contract RMNRemote_constructor is RMNRemoteTest {
+  function test_constructor_success() public {
+    assertEq(s_rmnRemote.getChainSelector(), 1);
   }
 }
