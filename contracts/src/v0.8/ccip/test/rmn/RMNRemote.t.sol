8779688bd6a4a90626383fc69431ad3c6705341a
Ryan Hall <hall.ryan.r@gmail.com>
2024-09-06 15:07:49 -0400

    create RMNRemote.t.sol and RMNHome.t.sol

---
+++ contracts/src/v0.8/ccip/test/rmn/RMNRemote.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.24;
+
+import {Internal} from "../../libraries/Internal.sol";
+import {BaseTest} from "../BaseTest.t.sol";
+
+contract RMNRemoteTest is BaseTest {
+  function setUp() public virtual override {
+    super.setUp();
+  }
+}
