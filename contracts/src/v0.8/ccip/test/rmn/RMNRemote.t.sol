177372794bcadf677bde7282e70b8c04f51c88c5
Ryan Hall <hall.ryan.r@gmail.com>
2024-09-09 13:11:15 -0400

    write setconfig tests

--- contracts/src/v0.8/ccip/test/rmn/RMNRemote.t.sol
+++ contracts/src/v0.8/ccip/test/rmn/RMNRemote.t.sol
@@ -29,5 +29,78 @@ contract RMNRemote_setConfig is RMNRemoteTest {
     RMNRemote.Config memory config =
       RMNRemote.Config({rmnHomeContractConfigDigest: _randomBytes32(), signers: s_signers, minSigners: 0});
     s_rmnRemote.setConfig(config);
+    RMNRemote.VersionedConfig memory versionedConfig = s_rmnRemote.getVersionedConfig();
+    assertEq(versionedConfig.config.minSigners, 0);
+  }
+
+  function test_setConfig_versionIncreases_success() public {
+    RMNRemote.Config memory config =
+      RMNRemote.Config({rmnHomeContractConfigDigest: _randomBytes32(), signers: s_signers, minSigners: 0});
+
+    vm.expectEmit();
+    emit RMNRemote.ConfigSet(RMNRemote.VersionedConfig({version: 1, config: config}));
+    s_rmnRemote.setConfig(config);
+    assertEq(s_rmnRemote.getVersionedConfig().version, 1);
+
+    vm.expectEmit();
+    emit RMNRemote.ConfigSet(RMNRemote.VersionedConfig({version: 2, config: config}));
+    s_rmnRemote.setConfig(config);
+    assertEq(s_rmnRemote.getVersionedConfig().version, 2);
+
+    vm.expectEmit();
+    emit RMNRemote.ConfigSet(RMNRemote.VersionedConfig({version: 3, config: config}));
+    s_rmnRemote.setConfig(config);
+    assertEq(s_rmnRemote.getVersionedConfig().version, 3);
+  }
+
+  function test_setConfig_addSigner_removeSigner_success() public {
+    RMNRemote.Config memory config =
+      RMNRemote.Config({rmnHomeContractConfigDigest: _randomBytes32(), signers: s_signers, minSigners: 0});
+    s_rmnRemote.setConfig(config);
+    RMNRemote.VersionedConfig memory versionedConfig = s_rmnRemote.getVersionedConfig();
+    // add a signer
+    s_signers.push(RMNRemote.Signer({onchainPublicKey: address(4), nodeIndex: 3}));
+    config = RMNRemote.Config({rmnHomeContractConfigDigest: _randomBytes32(), signers: s_signers, minSigners: 0});
+    s_rmnRemote.setConfig(config);
+    versionedConfig = s_rmnRemote.getVersionedConfig();
+    assertEq(versionedConfig.config.signers.length, 4);
+    assertEq(versionedConfig.config.signers[3].onchainPublicKey, address(4));
+    // remove signers
+    s_signers.pop();
+    s_signers.pop();
+    config = RMNRemote.Config({rmnHomeContractConfigDigest: _randomBytes32(), signers: s_signers, minSigners: 0});
+    s_rmnRemote.setConfig(config);
+    versionedConfig = s_rmnRemote.getVersionedConfig();
+    assertEq(versionedConfig.config.signers.length, 2);
+  }
+
+  function test_setConfig_invalidSignerOrder_reverts() public {
+    s_signers.push(RMNRemote.Signer({onchainPublicKey: address(4), nodeIndex: 4}));
+    s_signers.push(RMNRemote.Signer({onchainPublicKey: address(5), nodeIndex: 3}));
+    RMNRemote.Config memory config =
+      RMNRemote.Config({rmnHomeContractConfigDigest: _randomBytes32(), signers: s_signers, minSigners: 0});
+
+    vm.expectRevert(RMNRemote.InvalidSignerOrder.selector);
+    s_rmnRemote.setConfig(config);
+  }
+
+  function test_setConfig_minSignersTooHigh_reverts() public {
+    RMNRemote.Config memory config = RMNRemote.Config({
+      rmnHomeContractConfigDigest: _randomBytes32(),
+      signers: s_signers,
+      minSigners: uint64(s_signers.length + 1)
+    });
+
+    vm.expectRevert(RMNRemote.MinSignersTooHigh.selector);
+    s_rmnRemote.setConfig(config);
+  }
+
+  function test_setConfig_duplicateOnChainPublicKey_reverts() public {
+    s_signers.push(RMNRemote.Signer({onchainPublicKey: address(2), nodeIndex: 3}));
+    RMNRemote.Config memory config =
+      RMNRemote.Config({rmnHomeContractConfigDigest: _randomBytes32(), signers: s_signers, minSigners: 0});
+
+    vm.expectRevert(RMNRemote.DuplicateOnchainPublicKey.selector);
+    s_rmnRemote.setConfig(config);
   }
 }
