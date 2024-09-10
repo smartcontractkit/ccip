99aa66bfa258ed8f3fb7a306a099344ab8a081bd
Ryan Hall <hall.ryan.r@gmail.com>
2024-09-10 15:49:15 -0400

    refactor

--- contracts/src/v0.8/ccip/rmn/RMNRemote.sol
+++ contracts/src/v0.8/ccip/rmn/RMNRemote.sol
@@ -52,13 +52,77 @@ contract RMNRemote is OwnerIsCreator, ITypeAndVersion, IRMNV2 {
   Config s_config;
   uint32 s_configCount;
 
+  /// @notice An active curse on this subject will cause isCursed() to return true. Use this subject if there is an issue with a
+  /// remote chain, for which there exists a legacy lane contract deployed on the same chain as this RMN contract is
+  /// deployed, relying on isCursed().
+  bytes16 constant LEGACY_CURSE_SUBJECT = 0x01000000000000000000000000000000;
+
+  /// @notice An active curse on this subject will cause isCursed() and isCursed(bytes16) to return true. Use this subject for
+  /// issues affecting all of CCIP chains, or pertaining to the chain that this contract is deployed on, instead of using
+  /// the local chain selector as a subject.
+  bytes16 constant GLOBAL_CURSE_SUBJECT = 0x01000000000000000000000000000001;
+
   string public constant override typeAndVersion = "RMNRemote 1.6.0-dev";
   uint64 internal immutable i_chainSelector;
+
+  bytes16[] private s_cursedSubjectsSequence;
+  mapping(bytes16 subject => uint256 indexPlusOne) private s_cursedSubjectsIndexPlusOne;
   mapping(address signer => bool exists) s_signers; // for more gas efficient verify
 
   constructor(uint64 chainSelector) {
     i_chainSelector = chainSelector;
   }
+
+  // ================================================================
+  // │                         Verification                         │
+  // ================================================================
+
+  /// @notice Verifies signatures of RMN nodes, on dest lane updates as provided in the CommitReport
+  /// @param offrampAddress is not inferred by msg.sender, in case the call is made through ARMProxy
+  /// @param destLaneUpdates must be well formed, and is a representation of the CommitReport received from the oracles
+  /// @param signatures must be sorted in ascending order by signer address
+  /// @dev Will revert if verification fails. Needs to be called by the OffRamp for which the signatures are produced,
+  /// otherwise verification will fail.
+  function verify(
+    address offrampAddress,
+    Internal.MerkleRoot[] memory destLaneUpdates,
+    Signature[] memory signatures
+  ) external view {
+    if (s_configCount == 0) {
+      revert ConfigNotSet();
+    }
+
+    bytes32 signedHash = keccak256(
+      abi.encode(
+        RMN_V1_6_ANY2EVM_REPORT,
+        Report({
+          destChainId: block.chainid,
+          destChainSelector: i_chainSelector,
+          rmnRemoteContractAddress: address(this),
+          offrampAddress: offrampAddress,
+          rmnHomeContractConfigDigest: s_config.rmnHomeContractConfigDigest,
+          destLaneUpdates: destLaneUpdates
+        })
+      )
+    );
+
+    uint256 numSigners = 0;
+    address prevAddress = address(0);
+    for (uint256 i = 0; i < signatures.length; ++i) {
+      Signature memory sig = signatures[i];
+      address signerAddress = ecrecover(signedHash, 27, sig.r, sig.s);
+      if (signerAddress == address(0)) revert InvalidSignature();
+      if (!(prevAddress < signerAddress)) revert OutOfOrderSignatures();
+      if (!s_signers[signerAddress]) revert UnexpectedSigner();
+      prevAddress = signerAddress;
+      ++numSigners;
+    }
+    if (numSigners < s_config.minSigners) revert ThresholdNotMet();
+  }
+
+  // ================================================================
+  // │                            Config                            │
+  // ================================================================
 
   function setConfig(Config calldata newConfig) external onlyOwner {
     // sanity checks
@@ -104,71 +168,15 @@ contract RMNRemote is OwnerIsCreator, ITypeAndVersion, IRMNV2 {
     return VersionedConfig({version: s_configCount, config: s_config});
   }
 
-  /// @notice Verifies signatures of RMN nodes, on dest lane updates as provided in the CommitReport
-  /// @param offrampAddress is not inferred by msg.sender, in case the call is made through ARMProxy
-  /// @param destLaneUpdates must be well formed, and is a representation of the CommitReport received from the oracles
-  /// @param signatures must be sorted in ascending order by signer address
-  /// @dev Will revert if verification fails. Needs to be called by the OffRamp for which the signatures are produced,
-  /// otherwise verification will fail.
-  function verify(
-    address offrampAddress,
-    Internal.MerkleRoot[] memory destLaneUpdates,
-    Signature[] memory signatures
-  ) external view {
-    if (s_configCount == 0) {
-      revert ConfigNotSet();
-    }
-
-    bytes32 signedHash = keccak256(
-      abi.encode(
-        RMN_V1_6_ANY2EVM_REPORT,
-        Report({
-          destChainId: block.chainid,
-          destChainSelector: i_chainSelector,
-          rmnRemoteContractAddress: address(this),
-          offrampAddress: offrampAddress,
-          rmnHomeContractConfigDigest: s_config.rmnHomeContractConfigDigest,
-          destLaneUpdates: destLaneUpdates
-        })
-      )
-    );
-
-    uint256 numSigners = 0;
-    address prevAddress = address(0);
-    for (uint256 i = 0; i < signatures.length; ++i) {
-      Signature memory sig = signatures[i];
-      address signerAddress = ecrecover(signedHash, 27, sig.r, sig.s);
-      if (signerAddress == address(0)) revert InvalidSignature();
-      if (!(prevAddress < signerAddress)) revert OutOfOrderSignatures();
-      if (!s_signers[signerAddress]) revert UnexpectedSigner();
-      prevAddress = signerAddress;
-      ++numSigners;
-    }
-    if (numSigners < s_config.minSigners) revert ThresholdNotMet();
-  }
-
   /// @notice Returns the chain selector configured at deployment time
   /// @return chainSelector the chain selector (not the chain ID)
   function getChainSelector() external view returns (uint64 chainSelector) {
     return i_chainSelector;
   }
 
-  ///
-  /// Cursing support sketch
-  ///
-
-  // An active curse on this subject will cause isCursed() to return true. Use this subject if there is an issue with a
-  // remote chain, for which there exists a legacy lane contract deployed on the same chain as this RMN contract is
-  // deployed, relying on isCursed().
-  bytes16 constant LEGACY_CURSE_SUBJECT = 0x01000000000000000000000000000000;
-
-  // An active curse on this subject will cause isCursed() and isCursed(bytes16) to return true. Use this subject for
-  // issues affecting all of CCIP chains, or pertaining to the chain that this contract is deployed on, instead of using
-  // the local chain selector as a subject.
-  bytes16 constant GLOBAL_CURSE_SUBJECT = 0x01000000000000000000000000000001;
-
-  mapping(bytes16 subject => uint256 indexPlusOne) private s_cursedSubjectsIndexPlusOne;
-  bytes16[] private s_cursedSubjectsSequence;
+  // ================================================================
+  // │                           Cursing                            │
+  // ================================================================
 
   function ownerCurse(bytes16[] memory subjects) external onlyOwner {
     for (uint256 i = 0; i < subjects.length; ++i) {
