63ec29e7feec8e34a18af8ce61f131a8feabd3c7
Ryan Hall <hall.ryan.r@gmail.com>
2024-09-11 12:52:56 -0400

    add CCIPEncodingUtils contract and gethwrapper

---
+++ contracts/src/v0.8/ccip/CCIPEncodingUtils.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.24;
+
+import {RMNRemote} from "./rmn/RMNRemote.sol";
+
+/// @dev this file exposes structs that are otherwise internal to the CCIP codebase
+/// doing this allows those structs to be encoded and decoded with type safety in offchain code
+/// and tests because generated wrappers are available
+contract CCIPEncodingUtils {
+  constructor() {
+    revert("do not deploy");
+  }
+
+  /// @dev the RMN Report struct is used in integration / E2E tests
+  function _rmnReport(RMNRemote.Report memory rmnReport) external {}
+}
