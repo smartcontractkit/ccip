57da48d4e45524381dd6966c5bfc80e68f0f639f
Ryan Hall <hall.ryan.r@gmail.com>
2024-09-17 13:43:02 -0400

    update natspec

--- contracts/src/v0.8/ccip/interfaces/IRMNV2.sol
+++ contracts/src/v0.8/ccip/interfaces/IRMNV2.sol
@@ -14,7 +14,8 @@ interface IRMNV2 {
   /// @notice Verifies signatures of RMN nodes, on dest lane updates as provided in the CommitReport
   /// @param offRampAddress is not inferred by msg.sender, in case the call is made through ARMProxy
   /// @param merkleRoots must be well formed, and is a representation of the CommitReport received from the oracles
-  /// @param signatures must be sorted in ascending order by signer address
+  /// @param signatures rmnNodes ECDSA sigs, only r & s, must be sorted in ascending order by signer address
+  /// @param rawVs rmnNodes ECDSA sigs, part v bitmap
   /// @dev Will revert if verification fails
   function verify(
     address offRampAddress,
