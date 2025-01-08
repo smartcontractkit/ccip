994777048ddc38f6ac0292282400eca5b960f3c3
Dmytro Haidashenko <34754799+dhaidashenko@users.noreply.github.com>
2025-01-08 21:38:46 +0100

    Custom log index for sei chain (#1592)
    Patch with minor adjustments from the chainlink repo
    [PR](https://github.com/smartcontractkit/chainlink/pull/15858).
    Adjustments were caused by differences between repos. It's easier to
    do&review them than to apply all the changes made to chainlink repo that
    included MultiNode abstraction with 5k lines changed.

--- core/chains/evm/config/chaintype/chaintype.go
+++ core/chains/evm/config/chaintype/chaintype.go
@@ -17,6 +17,7 @@ const (
 	ChainMantle          ChainType = "mantle"
 	ChainMetis           ChainType = "metis"
 	ChainOptimismBedrock ChainType = "optimismBedrock"
+	ChainSei             ChainType = "sei"
 	ChainScroll          ChainType = "scroll"
 	ChainWeMix           ChainType = "wemix"
 	ChainXLayer          ChainType = "xlayer"
@@ -64,6 +65,8 @@ func FromSlug(slug string) ChainType {
 		return ChainMetis
 	case "optimismBedrock":
 		return ChainOptimismBedrock
+	case "sei":
+		return ChainSei
 	case "scroll":
 		return ChainScroll
 	case "wemix":
@@ -135,6 +138,7 @@ var ErrInvalid = fmt.Errorf("must be one of %s or omitted", strings.Join([]strin
 	string(ChainMantle),
 	string(ChainMetis),
 	string(ChainOptimismBedrock),
+	string(ChainSei),
 	string(ChainScroll),
 	string(ChainWeMix),
 	string(ChainXLayer),
